8e5f90a343c34f5bf9b6024b4520872d2f08729b
baron816 <baron.willeford@gmail.com>
2015-04-16 16:40:47 -0400

    fixes factory test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe User, type: :model do
-	it "creates a user" do
-		expect(build(:user)).to be_valid
-	end
+describe User do
+  it "factory creates a user" do
+  	expect(build(:user)).to be_valid
+  end
 end
