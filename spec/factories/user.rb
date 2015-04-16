6ce8e8592247902524432366d4aab6e3b32c4b49
baron816 <baron.willeford@gmail.com>
2015-04-16 17:36:33 -0400

    changes factory name to user with group

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		sequence(:name) { Faker::Name.name }
-		sequence(:email) { Faker::Internet.email }
-		password = Faker::Internet.password
-		sequence(:password) { password }
-		sequence(:password_confirmation) { password }
-		sequence(:age) { Faker::Number.number(2)}
+		factory :user_with_group do
+			sequence(:name) { Faker::Name.name }
+			sequence(:email) { Faker::Internet.email }
+			password = Faker::Internet.password
+			sequence(:password) { password }
+			sequence(:password_confirmation) { password }
+			sequence(:age) { Faker::Number.number(2)}
+		end
 	end
 end
