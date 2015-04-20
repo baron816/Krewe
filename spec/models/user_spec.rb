35226a4416dac7acb0b81b78da7fc2aecee8dafe
baron816 <baron.willeford@gmail.com>
2015-04-20 11:16:46 -0400

    adds test for groups association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe User do
   it "factory creates a user" do
-  	expect(build(:user_with_group)).to be_valid
+  	expect(create(:user_with_group)).to be_valid
+  end
+
+  before do
+  	@user = create(:user_with_group)
+  end
+  it "factory has 4 groups" do
+  	expect(@user.groups.length).to eq(4)
   end
 end
