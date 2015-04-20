f60cd3bf785777c4993b8aa329adb2c80b11a757
baron816 <baron.willeford@gmail.com>
2015-04-20 11:08:17 -0400

    fixes factory so that it works with association

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,18 @@
 FactoryGirl.define do
 	factory :user do
+		sequence(:name) { Faker::Name.name }
+		sequence(:email) { Faker::Internet.email }
+		password = Faker::Internet.password
+		sequence(:password) { password }
+		sequence(:password_confirmation) { password }
+		sequence(:age) { Faker::Number.number(2)}
+
 		factory :user_with_group do
-			sequence(:name) { Faker::Name.name }
-			sequence(:email) { Faker::Internet.email }
-			password = Faker::Internet.password
-			sequence(:password) { password }
-			sequence(:password_confirmation) { password }
-			sequence(:age) { Faker::Number.number(2)}
+			
+
+			after(:create) do |user, evaluator|
+				create_list(:user_group, 4, user: user)
+			end
 		end
 	end
 end
