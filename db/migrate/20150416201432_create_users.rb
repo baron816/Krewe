1c6ca0ef1e2ebdd5aa56675a587bd05a0f12a451
baron816 <baron.willeford@gmail.com>
2015-04-20 21:55:59 -0400

    temporarily removes non nil requirements and adds address and long, lat fields

--- db/migrate/20150416201432_create_users.rb
+++ db/migrate/20150416201432_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name, null: false
-    	t.string :password_digest, null: false
-    	t.string :email, null: false
+    	t.string :name
+    	t.string :password_digest
+    	t.string :email
     	t.integer :age
+    	t.string :address
+    	t.float :latitude
+    	t.float :longitude
 
     	t.timestamps
     end
