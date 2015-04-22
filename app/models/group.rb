c83712adb577f61e6bea5dbb7528e741ba7e49c2
baron816 <baron.willeford@gmail.com>
2015-04-22 11:11:35 -0400

    adds method for finding first user and setting location

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,20 @@
 class Group < ActiveRecord::Base
 	has_many :user_groups
 	has_many :users, through: :user_groups
+	
+	before_create do
+		find_first_user
+		set_coordinates
+	end
+
+	def find_first_user
+		self.users << User.find_by(searching: true)
+	end
+
+	def set_coordinates
+		first_user = self.users.first
+		self.longitude = first_user.longitude
+		self.latitude = first_user.latitude
+	end
+
 end
