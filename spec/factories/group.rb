3cadebf20399c417a85b81686753466309caf579
baron816 <baron.willeford@gmail.com>
2015-05-05 15:17:40 -0400

    no longer users group with user factory

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,15 +1,6 @@
 FactoryGirl.define do
 
 	factory :group do
-
-		factory :group_with_user do
-			ignore do
-				user_count 3
-			end
-
-			after(:create) do |group, evaluator|
-				create_list(:user_group, evaluator.user_count, group: group)
-			end
-		end
+ 
 	end
 end
