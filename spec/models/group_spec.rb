638385c3c2fc1a1bdaa5f88498db826c84662d0d
baron816 <baron.willeford@gmail.com>
2015-05-05 17:21:40 -0400

    adds test to check that new groups only find users who are searching

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -22,14 +22,31 @@ describe Group do
 		before do
 		  create(:user_home)
 		  create(:user_wtc)
+		  sleep(0.4)
 		  create(:user_121)
+		  sleep(0.4)
 		  create(:user_dbc)
+		  sleep(0.4)
 		  create(:user_stucco)
+		  sleep(0.4)
+		  create(:user_8th_st)
+		  sleep(0.4)
+		  create(:user_gd)
 		  @group = create(:group)
+		  @group2 = create(:group)
+		  @group3 = create(:group)
 		end
 
 		it "group has first four users. fifth is out of range" do
 			expect(@group.users.length).to eq(4)
+		end
+
+		it "second group only has one user" do
+			expect(@group2.users.length).to eq(2)
+		end
+
+		it "third group doesn't find anyone but the original user" do
+			expect(@group3.users.length).to eq(1)
 		end
 	end
 end
