9fd360d5572dbec47e5146736eab428fc0f966d2
baron816 <baron.willeford@gmail.com>
2015-05-05 15:16:19 -0400

    reverse geocodes and adds users after initial user

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ActiveRecord::Base
 	before_create do
 		find_first_user
 		set_coordinates
+		get_more_users
 	end
 
 	def find_first_user
@@ -17,4 +18,14 @@ class Group < ActiveRecord::Base
 		self.latitude = first_user.latitude
 	end
 
+	def get_more_users
+		if self.users.length < 6
+			found_users = User.near(self, 2).limit(5)
+			found_users.each do |user|
+				 self.users << user unless self.users.include?(user) 
+			end
+		end
+	end
+
+	reverse_geocoded_by :latitude, :longitude
 end
