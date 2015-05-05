04a622cb4b7b41fe183843030ed8407ff9381db1
baron816 <baron.willeford@gmail.com>
2015-05-05 15:20:57 -0400

    adds test for coordinates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,25 @@ describe User do
   	expect(create(:user_with_group)).to be_valid
   end
 
-  before do
-  	@user = create(:user_with_group)
+
+  context "User home" do
+	before do
+  		@user = create(:user_home)
+	end
+
+	it "user home has address" do
+  		expect(@user.address).to eq('135 William Street, New York, NY')
+  	end  	
+
+  	it "user home has longitude coordinate" do
+  		expect(@user.longitude).to eq(-74.00671419999999)
+  	end
+
+  	it "user home has latitude coordinate" do
+  		expect(@user.latitude).to eq(40.7094706)
+  	end
   end
-  it "factory has 4 groups" do
-  	expect(@user.groups.length).to eq(4)
-  end
+
+
+
 end
