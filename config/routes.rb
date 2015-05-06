5cc2555f53d95d793ed0171435213105e4010272
baron816 <baron.willeford@gmail.com>
2015-05-06 17:59:35 -0400

    adds routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :users
 
   resources :groups, only: [:index, :show]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
