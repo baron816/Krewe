2d2aa0803a292bfdee8d50bda141546c47c59eb2
baron816 <baron.willeford@gmail.com>
2015-05-06 21:25:28 -0400

    adds eager loading of groups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
 	private
 	def set_user
-		@user = User.find(params[:id])
+		@user = User.includes(:groups).find(params[:id])
 	end
 
 	def user_params
