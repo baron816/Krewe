b8b913fe9445470cbaa8a51d585048287cd93e11
baron816 <baron.willeford@gmail.com>
2015-05-08 16:00:20 -0400

    adds methods for determining users age

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ActiveRecord::Base
 	after_validation :geocode
 
 	def address
-		[street, city, state].join(',')
+		[street, city, state].join(', ')
+	end
+
+	def birthday
+		Date.new(birth_year, birth_month, birth_day)
+	end
+
+	def age
+		now = Date.today
+		now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
 	end
 end
