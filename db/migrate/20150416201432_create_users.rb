d5676b141281c1b08a4be7a59949d000af17e4df
baron816 <baron.willeford@gmail.com>
2015-05-08 13:54:30 -0400

    does not allow false values for most attributes

--- db/migrate/20150416201432_create_users.rb
+++ db/migrate/20150416201432_create_users.rb
@@ -1,12 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name
-    	t.string :password_digest
-    	t.string :email
-    	t.integer :age
+    	t.string :name, null: false
+    	t.string :password_digest, null: false
+    	t.string :email, null: false
+    	t.integer :birth_day, null: false
+        t.integer :birth_month, null: false
+        t.integer :birth_year, null: false
         t.boolean :searching, default: true
-    	t.string :address
+    	t.string :street, null: false
+        t.string :city, null: false
+        t.string :state, null: false
     	t.float :latitude
     	t.float :longitude
 
