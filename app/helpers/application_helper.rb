d41abce4333b2c204769b4d4a267bd72e9758e64
baron816 <baron.willeford@gmail.com>
2015-05-08 15:59:35 -0400

    adds helpers to display in new user dropdowns for state and birhtday

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,83 @@
 module ApplicationHelper
+	def us_states
+	    [
+	      ['Alabama', 'AL'],
+	      ['Alaska', 'AK'],
+	      ['Arizona', 'AZ'],
+	      ['Arkansas', 'AR'],
+	      ['California', 'CA'],
+	      ['Colorado', 'CO'],
+	      ['Connecticut', 'CT'],
+	      ['Delaware', 'DE'],
+	      ['District of Columbia', 'DC'],
+	      ['Florida', 'FL'],
+	      ['Georgia', 'GA'],
+	      ['Hawaii', 'HI'],
+	      ['Idaho', 'ID'],
+	      ['Illinois', 'IL'],
+	      ['Indiana', 'IN'],
+	      ['Iowa', 'IA'],
+	      ['Kansas', 'KS'],
+	      ['Kentucky', 'KY'],
+	      ['Louisiana', 'LA'],
+	      ['Maine', 'ME'],
+	      ['Maryland', 'MD'],
+	      ['Massachusetts', 'MA'],
+	      ['Michigan', 'MI'],
+	      ['Minnesota', 'MN'],
+	      ['Mississippi', 'MS'],
+	      ['Missouri', 'MO'],
+	      ['Montana', 'MT'],
+	      ['Nebraska', 'NE'],
+	      ['Nevada', 'NV'],
+	      ['New Hampshire', 'NH'],
+	      ['New Jersey', 'NJ'],
+	      ['New Mexico', 'NM'],
+	      ['New York', 'NY'],
+	      ['North Carolina', 'NC'],
+	      ['North Dakota', 'ND'],
+	      ['Ohio', 'OH'],
+	      ['Oklahoma', 'OK'],
+	      ['Oregon', 'OR'],
+	      ['Pennsylvania', 'PA'],
+	      ['Puerto Rico', 'PR'],
+	      ['Rhode Island', 'RI'],
+	      ['South Carolina', 'SC'],
+	      ['South Dakota', 'SD'],
+	      ['Tennessee', 'TN'],
+	      ['Texas', 'TX'],
+	      ['Utah', 'UT'],
+	      ['Vermont', 'VT'],
+	      ['Virginia', 'VA'],
+	      ['Washington', 'WA'],
+	      ['West Virginia', 'WV'],
+	      ['Wisconsin', 'WI'],
+	      ['Wyoming', 'WY']
+	    ]
+	end
+
+	def month_days
+		(1..31).to_a
+	end
+
+	def month_names
+		[
+			['January', 1],
+			['February', 2],
+			['March', 3],
+			['April', 4],
+			['May', 5],
+			['June', 6],
+			['July', 7],
+			['August', 8],
+			['September', 9],
+			['October', 10],
+			['Novermber', 11],
+			['December', 12]
+		]
+	end
+
+	def years
+		(1900..1999).to_a.reverse
+	end
 end
