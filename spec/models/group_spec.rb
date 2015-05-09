988e9c94227941e586c3c41864ce76749df40d5e
baron816 <baron.willeford@gmail.com>
2015-05-09 09:29:49 -0400

    adds more sleep time to prevent too many api calls in a row

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -22,15 +22,15 @@ describe Group do
 		before do
 		  create(:user_home)
 		  create(:user_wtc)
-		  sleep(0.4)
+		  sleep(0.6)
 		  create(:user_121)
-		  sleep(0.4)
+		  sleep(0.6)
 		  create(:user_dbc)
-		  sleep(0.4)
+		  sleep(0.6)
 		  create(:user_stucco)
-		  sleep(0.4)
+		  sleep(0.6)
 		  create(:user_8th_st)
-		  sleep(0.4)
+		  sleep(0.6)
 		  create(:user_gd)
 		  @group = create(:group)
 		  @group2 = create(:group)
@@ -50,11 +50,11 @@ describe Group do
 		end
 
 		it "first group adds more users, but doesn't go over the limit" do
-			sleep(0.4)
+			sleep(0.6)
 			create(:user_138)
-			sleep(0.4)
+			sleep(0.6)
 			create(:user_122)
-			sleep(0.4)
+			sleep(0.6)
 			create(:user_130)
 
 			@group.get_more_users
