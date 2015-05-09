31156682656ddb46695f8a7c450fab3e425cd74a
baron816 <baron.willeford@gmail.com>
2015-05-09 09:34:28 -0400

    adds additional test to check associations

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,20 @@ describe "Message" do
   it "builds a message" do
   	expect(build(:message)).to be_valid
   end
+
+  before do
+    @message = create(:message)
+  end
+
+  it "has string content" do
+  	expect(@message.content).to be_a(String)
+  end
+
+  it "has a user" do
+  	expect(@message.user).to be_a(User)
+  end
+
+  it "has a group" do
+  	expect(@message.group).to be_a(Group)
+  end
 end
