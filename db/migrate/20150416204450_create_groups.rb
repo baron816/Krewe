776485bf20d01d7def1a3472d6f21456d38555cd
baron816 <baron.willeford@gmail.com>
2015-05-09 17:08:34 -0400

    sets default user limit and creates can join field

--- db/migrate/20150416204450_create_groups.rb
+++ db/migrate/20150416204450_create_groups.rb
@@ -4,7 +4,8 @@ class CreateGroups < ActiveRecord::Migration
     	t.string :name
     	t.float :longitude
     	t.float :latitude
-    	t.integer :user_limit
+    	t.integer :user_limit, default: 6
+    	t.boolean :can_join, default: true
     	t.timestamps
     end
   end
