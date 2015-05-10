8e52c2cdfaac3abed02f8b9f54f634ffd3eb7ccf
baron816 <baron.willeford@gmail.com>
2015-05-10 15:48:30 -0400

    creates separate variable for messages and paginates them

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
 
 	def show
 		@message = Message.new
-		@group = Group.includes(:users, :messages).find(params[:id])
+		@group = Group.includes(:users).find(params[:id])
+		@messages = Message.paginate(page: params[:page], per_page: 5)
 	end
 end
