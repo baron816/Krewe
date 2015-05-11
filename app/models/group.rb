1790a0edfb5c6026b8f5631b425dd4cffbf47e40
baron816 <baron.willeford@gmail.com>
2015-05-11 19:45:12 -0400

    removes commented method

--- app/models/group.rb
+++ app/models/group.rb
@@ -19,16 +19,5 @@ class Group < ActiveRecord::Base
 		self.can_join = self.users.count < self.user_limit
 	end
 
-	# def get_more_users
-	# 	if self.users.length < 6
-	# 		found_users = User.near(self, 2).where(searching: true).limit(5)
-	# 		found_users.each do |user|
-	# 			 self.users << user unless self.users.include?(user) || self.users.length == 6
-	# 			 user.searching = false
-	# 			 user.save! 
-	# 		end
-	# 	end
-	# end
-
 	reverse_geocoded_by :latitude, :longitude
 end
