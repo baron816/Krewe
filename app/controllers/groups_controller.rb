f69bcebb9f7406f336948ca5d6eead2ccad5c4ca
baron816 <baron.willeford@gmail.com>
2015-05-21 14:45:25 -0400

    adds method for dropping user from a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,10 @@ class GroupsController < ApplicationController
 		@group = Group.includes(:users).find(params[:id])
 		@messages = Message.paginate(page: params[:page], per_page: 5)
 	end
+
+	def drop
+		@group = Group.includes(:users).find(params[:id])
+		@group.drop_user(current_user)
+		redirect_to user_path(current_user)
+	end
 end
