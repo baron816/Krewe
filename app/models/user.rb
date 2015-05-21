3f03cf4063eb4ac162566345c8564e3d4e50e3a6
baron816 <baron.willeford@gmail.com>
2015-05-21 11:30:22 -0400

    adds validations for remaining fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, length: { minimum: 3 }
+	validates :email, presence: true, uniqueness: true
+	validates :street, presence: true
+	validates :city, presence: true
+	validates :category, presence: true
+	validates :password, presence: true, length: { minimum: 6 }
 
 	has_secure_password
 	has_many :user_groups
