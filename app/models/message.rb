cc8c891356b7d380b0ad28d0296828d49b629f38
baron816 <baron.willeford@gmail.com>
2015-05-24 12:17:21 -0400

    adds method to create notifications on create

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,18 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :group
+
+	after_create do	
+		send_notifications
+	end
+
+	def say
+		self.group
+	end
 	
+	def send_notifications
+		self.group.users.each do |user|
+			Notification.create(group_id: self.group.id, user_id: user.id, poster_id: self.user.id)
+		end
+	end
 end
