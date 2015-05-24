844f02f4e21f32a4b414f8dfe25fe93b5cc2d270
baron816 <baron.willeford@gmail.com>
2015-05-24 12:16:32 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509015331) do
+ActiveRecord::Schema.define(version: 20150524152534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20150509015331) do
     t.integer  "group_id"
     t.integer  "user_id"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.integer  "poster_id"
+    t.boolean  "viewed",     default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
