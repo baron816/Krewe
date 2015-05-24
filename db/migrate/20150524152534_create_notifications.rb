b57541bd50684bab9ee5b03f3deabc32f6c49011
baron816 <baron.willeford@gmail.com>
2015-05-24 12:15:49 -0400

    adds notification migration

---
+++ db/migrate/20150524152534_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration
+  def change
+    create_table :notifications do |t|
+    	t.references :group
+    	t.references :user
+    	t.references :poster
+    	t.boolean :viewed, default: false
+
+    	t.timestamps
+    end
+  end
+end
