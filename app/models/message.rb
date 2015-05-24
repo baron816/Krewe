7586949e9ce8a380a714291f3ec8cbe613ae8635
baron816 <baron.willeford@gmail.com>
2015-05-24 13:22:13 -0400

    doesnt create notification for user posting

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,7 @@ class Message < ActiveRecord::Base
 	
 	def send_notifications
 		self.group.users.each do |user|
-			Notification.create(group_id: self.group.id, user_id: user.id, poster_id: self.user.id)
+			Notification.create(group_id: self.group.id, user_id: user.id, poster_id: self.user.id) unless user == self.user
 		end
 	end
 end
