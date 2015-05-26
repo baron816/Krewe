d62a2aec7f3ecb3f24ca956a7b95be2059aa84b8
baron816 <baron.willeford@gmail.com>
2015-05-26 19:46:15 -0400

    adds validation for message length

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :group
 
+	validates :content, presence: true, length: { minimum: 3 }
+
 	after_create do	
 		send_notifications
 	end
