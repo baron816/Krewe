900b7181f6062fabd2a376d372db3d295bf91297
baron816 <baron.willeford@gmail.com>
2015-05-27 18:28:59 -0400

    removes whitespace

--- app/assets/javascripts/sidereal.js
+++ app/assets/javascripts/sidereal.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	$('#message_content').on("keyup", function(){
 		if ($(this).val().length < 3) {
 			$('#new-message-submit').addClass('disabled')
-
 		} else {
 			$('#new-message-submit').removeClass('disabled')
 		};
