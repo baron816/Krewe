45b98a1043c5e32716e6ab7b81aa9ac5c4e4a423
baron816 <baron.willeford@gmail.com>
2015-05-27 11:49:04 -0400

    notifications will be sent after sending message

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -1,4 +1,12 @@
 class PersonalMessage < ActiveRecord::Base
 	belongs_to :sender, class_name: 'User'
 	belongs_to :receiver, class_name: 'User'
+
+	after_create do
+		send_notification
+	end
+
+	def send_notification
+		Notification.create(user: self.receiver, poster: self.sender, category: "Personal")
+	end
 end
