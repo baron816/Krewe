5a59ac163c240f2e6ea19d4f8459d09d43b0faca
baron816 <baron.willeford@gmail.com>
2015-05-26 20:16:47 -0400

    adds jquery function to check if message is longer than 3

---
+++ app/assets/javascripts/sidereal.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#message_content').on("keyup", function(){
+		if ($(this).val().length < 3) {
+			$('#new-message-submit').addClass('disabled')
+
+		} else {
+			$('#new-message-submit').removeClass('disabled')
+		};
+	})
+})
