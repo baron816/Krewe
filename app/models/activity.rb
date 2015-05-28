60deb4ab666f6e6768cd602d34219ae3c7b882e4
baron816 <baron.willeford@gmail.com>
2015-05-28 17:05:19 -0400

    adds method to determine if user is already going

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,8 @@ class Activity < ActiveRecord::Base
 	belongs_to :proposer, class_name: "User"
 	has_many :user_activities
 	has_many :users, through: :user_activities
+
+	def user_going?(user)
+		self.users.include?(user)
+	end
 end
