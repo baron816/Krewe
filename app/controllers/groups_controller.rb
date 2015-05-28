3de1cd603dfd2b81af4cc64242f3dfb7c23847a0
baron816 <baron.willeford@gmail.com>
2015-05-28 15:13:35 -0400

    eager loads activities

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 	end
 
 	def show
-		@group = Group.includes(:users, :notifications).find(params[:id])
+		@group = Group.includes(:users, :notifications, :activities).find(params[:id])
 		redirect_to user_path(current_user) unless @group.users.include?(current_user)
 		@group.dismiss_notifications(current_user)
 		@message = Message.new
