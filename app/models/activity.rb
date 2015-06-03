3fb998cb8b3ac2eab750ac31bae6ccf8991ce084
baron816 <baron.willeford@gmail.com>
2015-06-03 17:45:09 -0400

    adds validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,9 @@ class Activity < ActiveRecord::Base
 	has_many :user_activities
 	has_many :users, through: :user_activities
 
+	validates :plan, presence: true, length: { minimum: 3 }
+	validates :appointment, presence: true
+
 	def user_going?(user)
 		self.users.include?(user)
 	end
