2c0b4dea054a6e46aeee3f64e72721d70e48f441
baron816 <baron.willeford@gmail.com>
2015-06-03 20:46:49 -0400

    removes unnecessary self

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,7 +4,7 @@ class Notification < ActiveRecord::Base
 	belongs_to :poster, class_name: 'User'
 
 	def dismiss
-		self.viewed = true if self.viewed == false
+		self.viewed = true if viewed == false
 		save
 	end
 end
