c9112f3633c45c200868763549a7d4b0d199fc41
baron816 <baron.willeford@gmail.com>
2015-06-12 12:26:25 -0400

    fixes method name for dismiss notificaitoin

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
 	def show
 		@group = Group.includes(:users, :notifications, :activities).find(params[:id])
 		redirect_to user_path(current_user) unless @group.users.include?(current_user)
-		@group.dismiss_notifications(current_user)
+		current_user.dismiss_group_notifications(@group)
 		@message = Message.new
 		@messages = @group.messages.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
 		@activity = Activity.new
