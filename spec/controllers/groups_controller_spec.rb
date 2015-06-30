bed37b0fb8e2d72afc504a085b4b3cfc0bb93f9d
baron816 <baron.willeford@gmail.com>
2015-06-29 20:46:37 -0400

    adds drop group tests

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe GroupsController do
+  before do
+    @user = create(:user_home)
+	@group = @user.groups.first
+	session[:user_id] = @user
+  end
+
   describe "GET #show" do
   	before do
-  	  @user = create(:user_home)
-  	  @group = @user.groups.first
-  	  session[:user_id] = @user
   	  get :show, id: @group
   	end
 
@@ -23,6 +26,17 @@ describe GroupsController do
 
     it "has a new activity" do
     	expect(assigns[:activity]).to be_a_new(Activity)
+    end
+  end
+
+  describe "DELETE #drop_user" do
+  	it "finds the correct group" do
+  		delete :drop_user, group_id: @group
+  		expect(assigns[:group]).to eq(@group)
+  	end
+
+    it "drops the user's group" do
+    	expect { delete :drop_user, group_id: @group }.to change(@user.groups, :count).by(-1)
     end
   end
 end
