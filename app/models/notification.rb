51a918e7bc9d7126c895350593871a1087c7cc5a
baron816 <baron.willeford@gmail.com>
2015-07-09 09:33:48 -0400

    adds methods from user

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -20,23 +20,58 @@ class Notification < ActiveRecord::Base
 		save
 	end
 
-	def self.unviewed_personal_notifications_from_user(user)
-		unviewed_category_notifications("PersonalMessage").poster_notifications(user)
+	def self.unviewed_notifications_count
+		unviewed_notifications.count
 	end
 
 	def self.unviewed_category_notifications(category)
 		unviewed_notifications.category_notifications(category)
 	end
+	
+	#personal
+
+	def self.unviewed_personal_notifications_from_user(user)
+		unviewed_category_notifications("PersonalMessage").poster_notifications(user)
+	end
+
+	def self.unviewed_personal_notifications_from_user_count(user)
+		personal_count = unviewed_personal_notifications_from_user(user).count
+		personal_count if personal_count > 0
+	end
+
+	def self.dismiss_personal_notifications_from_user(user)
+		personal_notes = unviewed_personal_notifications_from_user(user)
+		personal_notes.each(&:dismiss) if personal_notes.any?
+	end
+
+	#group
 
 	def self.unviewed_group_notifications_from_group(group)
 		unviewed_category_notifications("Group").notifiable_notifications(group)
+	end
+
+	def self.unviewed_group_notification_count(group)
+		group_count = unviewed_group_notifications_from_group(group).count + unviewed_message_notifications_from_group(group).count
+		group_count if group_count > 0
 	end
 
 	def self.unviewed_message_notifications_from_group(group)
 		unviewed_notifications.includes(:message).where("messages.group_id" => group)
 	end
 
+	def self.dismiss_group_notifications_from_group(group)
+		group_notes = unviewed_group_notifications_from_group(group) + unviewed_message_notifications_from_group(group)
+		group_notes.each(&:dismiss) if group_notes.any?
+	end
+
+	#activity
+
 	def self.unviewed_activity_notifications(activity)
 		unviewed_notifications.notifiable_notifications(activity)
+	end
+
+	def self.dismiss_activity_notification(activity)
+		activity_note = unviewed_activity_notifications(activity).take
+		activity_note.dismiss if activity_note
 	end
 end
