c4634527e36b4a4155bf6e3741a8baf47d82f0d2
baron816 <baron.willeford@gmail.com>
2015-07-10 12:41:52 -0400

    adds password reset fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709172207) do
+ActiveRecord::Schema.define(version: 20150710141229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,22 +84,24 @@ ActiveRecord::Schema.define(version: 20150709172207) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                           null: false
-    t.string   "password_digest",                null: false
-    t.string   "email",                          null: false
-    t.string   "street",                         null: false
-    t.string   "city",                           null: false
-    t.string   "state",                          null: false
-    t.string   "category",                       null: false
+    t.string   "name",                                null: false
+    t.string   "password_digest",                     null: false
+    t.string   "email",                               null: false
+    t.string   "street",                              null: false
+    t.string   "city",                                null: false
+    t.string   "state",                               null: false
+    t.string   "category",                            null: false
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "sign_in_count",     default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "last_sign_in_at"
     t.inet     "last_sign_in_ip"
     t.string   "auth_token"
-    t.integer  "dropped_group_ids", default: [],              array: true
+    t.integer  "dropped_group_ids",      default: [],              array: true
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
