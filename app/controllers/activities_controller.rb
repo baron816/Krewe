e4bc7735964067c357adabddd4ce37b72e1cb001
baron816 <baron.willeford@gmail.com>
2015-07-11 11:13:29 -0400

    fixes up callback methods and adds edit and update methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
-	before_action :set_group, only: [:create, :new]
-	before_action :set_activity, only: [:add_user, :remove_user]
+	before_action :set_group, only: [:create, :new, :edit]
+	before_action :set_activity_with_activity_id, only: [:add_user, :remove_user]
+	before_action :set_activity, only: [:show, :edit, :update]
 
 	def create
 		@activity = @group.activities.new(activity_params)
@@ -28,8 +29,21 @@ class ActivitiesController < ApplicationController
 	end
 
 	def show
-		@activity = Activity.find(params[:id])
 		@notifications.dismiss_activity_notification(@activity)
+	end
+
+	def edit
+	end
+
+	def update
+		if @activity.update(activity_params) && @activity.proposed_by(current_user)
+			redirect_to group_activity_path(@activity.group, @activity)
+		else
+			render :edit
+		end		
+	end
+
+	def show
 	end
 
 	def new
@@ -37,8 +51,12 @@ class ActivitiesController < ApplicationController
 	end
 
 	private
-	def set_activity
+	def set_activity_with_activity_id
 		@activity = Activity.find(params[:activity_id])
+	end
+
+	def set_activity
+		@activity = Activity.find(params[:id])
 	end
 
 	def set_group
