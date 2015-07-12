3d042a0b771b1d7e4b5dc4788452f37b0c9dcc7c
baron816 <baron.willeford@gmail.com>
2015-07-12 19:29:29 -0400

    adds validations for field lengths and email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, length: { minimum: 3 }
-	validates :email, presence: true, uniqueness: true
+	validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 	validates :password, presence: true, length: { minimum: 6 }, on: :create
 	validates_presence_of :longitude, :street, :city, :category
 
