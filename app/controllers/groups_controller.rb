7b882fd5ebb5d884b79289b7e1b5df863ba4fcc8
baron816 <baron.willeford@gmail.com>
2015-07-13 10:05:32 -0400

    breaks group users into separate variable

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,8 @@
 class GroupsController < ApplicationController
 	def show
-		@group = Group.includes(:users, :activities).find(params[:id])
-		redirect_to user_path(current_user) unless @group.users.include?(current_user)
+		@group = Group.includes(:activities).find(params[:id])
+		@users = @group.users
+		redirect_to user_path(current_user) unless @users.include?(current_user)
 		@notifications.dismiss_group_notifications_from_group(@group)
 		@messages = @group.messages.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
 		@message = Message.new
