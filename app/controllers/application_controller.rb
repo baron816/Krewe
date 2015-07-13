4b39ab3ba243dd051519fb43800d85bc545d300e
baron816 <baron.willeford@gmail.com>
2015-07-12 21:38:47 -0400

    adds method to check that a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,13 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :set_notifications, only: [:show, :new, :edit, :public_profile, :index]
+  before_action :user_logged?, except: [:new, :index, :create, :edit, :update]
 
   def set_notifications
   	@notifications = current_user.notifications if current_user
+  end
+
+  def user_logged?
+    redirect_to root_path unless current_user
   end
 end
