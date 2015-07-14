10afc9acddfeed9cd4f6aa2703e0677dce96ef4d
baron816 <baron.willeford@gmail.com>
2015-07-14 10:57:10 -0400

    changed to let notation and adds sleep

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe Group do
 	before do
 		create(:user_home)
+		sleep(0.6)
 		create(:user_wtc)
-		@group1 = Group.first
+		sleep(0.6)
 	end
-	
-	context "two users" do	
-		it	'has 2 user' do
-			@group1 = Group.first
-			expect(@group1.users.length).to eq(2)
-		end
+
+	let(:group1) { Group.first }
+
+	it	'has 2 user' do
+		expect(group1.users.count).to eq(2)
 	end
 
 	context "more users" do
@@ -26,46 +26,46 @@ describe Group do
 		  create(:user_8th_st)
 		  sleep(0.6)
 		  create(:user_gd)
-		  @group2 = Group.second
-		  @group3 = Group.third
 		end
+		  let(:group2) { Group.second }
+		  let(:group3) { Group.third }
 
 		it "group has first four users. fifth is out of range" do
-			expect(@group1.users.length).to eq(4)
+			expect(group1.users.length).to eq(4)
 		end
 
 		it "second group only has one user" do
-			expect(@group2.users.length).to eq(2)
+			expect(group2.users.length).to eq(2)
 		end
 
 		it "third group doesn't find anyone but the original user" do
-			expect(@group3.users.length).to eq(1)
+			expect(group3.users.length).to eq(1)
 		end
 
 		context "when group not full" do
 			describe "#drop_user" do
 			  it "correctly removes a user" do
-			  	@group1.drop_user(User.first)
-			  	expect(@group1.users.count).to eq(3) 
+			  	group1.drop_user(User.first)
+			  	expect(group1.users.count).to eq(3)
 			  end
 
 			  context "description" do
-			  	before do  
-			  	sleep(0.6)
-	  	        create(:user_138)
+				  before do
+						sleep(0.6)
+		  	    create(:user_138)
 		        sleep(0.6)
 		        create(:user_122)
 		        sleep(0.6)
 		        create(:user_130)
-			  	end
-			  	
+					end
+
 				  it "still adds users to first group" do
-			        expect(@group1.users.count).to eql(6)
+			        expect(group1.users.count).to eql(6)
 				  end
 
 				  it "resets can_join to true" do
-				  	@group1.drop_user(User.first)
-				  	expect(@group1.can_join).to be true
+				  	group1.drop_user(User.first)
+				  	expect(group1.can_join).to be true
 				  end
 			  end
 
@@ -73,5 +73,5 @@ describe Group do
 		end
 
 	end
-	
+
 end
