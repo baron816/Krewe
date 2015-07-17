945827833879c7f1c7c437e9107824814ad2bf8c
baron816 <baron.willeford@gmail.com>
2015-07-17 19:11:40 -0400

    searches for location on change of form

--- app/assets/javascripts/sidereal.js
+++ app/assets/javascripts/sidereal.js
@@ -10,4 +10,23 @@ $(document).ready(function(){
 	$('#datetimepicker').datetimepicker({
 		format: 'YYYY-MM-DD hh:mm A'
 	});
+
+	$('#address-info').change( function () {
+		codeAddress()
+	});
 })
+
+function codeAddress() {
+	geocoder = new google.maps.Geocoder()
+	var address = $('#user_street').val() + ", " + $('#user_city').val() + ", " + $('#user_state').val();
+	geocoder.geocode( { 'address': address}, function (results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			var lat = results[0].geometry.location.lat()
+			var lng = results[0].geometry.location.lng()
+			$('#lat').val(lat)
+			$('#lng').val(lng)
+		} else {
+			console.log("error" + status);
+		}
+	})
+}
