22618a9c89aaad76ede99bb855171e530a8209db
baron816 <baron.willeford@gmail.com>
2015-07-22 14:51:35 -0400

    future activities called through group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,7 @@ class GroupsController < ApplicationController
 		redirect_to user_path(current_user) unless @users.include?(current_user)
 		@notifications.dismiss_group_notifications_from_group(@group)
 		@messages = @group.messages.includes(:user).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
-		@activities = @group.upcoming_activities.includes(:users)
+		@activities = @group.activities.future_activities.includes(:users)
 		@message = Message.new
 	end
 
