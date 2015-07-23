1bb02dd6efa9a0d87af44cfdf1b8b04d9a4199ec
baron816 <baron.willeford@gmail.com>
2015-07-23 17:08:02 -0400

    no longer sets notifications variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :set_notifications, only: [:show, :new, :edit, :public_profile, :index]
   before_action :user_logged?, except: [:new, :index, :create, :edit, :update]
-
-  def set_notifications
-  	@notifications = current_user.notifications.includes(:poster) if current_user
-  end
 
   def user_logged?
     redirect_to root_path unless current_user
