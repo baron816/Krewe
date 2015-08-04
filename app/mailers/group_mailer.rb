c1be3b044de8c4c8f3c9ee2f8ddb1597e1f861c2
baron816 <baron.willeford@gmail.com>
2015-08-04 15:37:07 -0400

    adds group mailer and join_group method to send emails when a user joins

---
+++ app/mailers/group_mailer.rb
@@ -0,0 +1,15 @@
+class GroupMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.group_mailer.join_group.subject
+  #
+  def join_group(params = {})
+    @user = params[:user]
+    @group = params[:group]
+    @poster = params[:poster]
+
+    mail to: @user.email, subject: "Sidereal: #{@poster.name} joined group #{@group.name}"
+  end
+end
