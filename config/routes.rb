c20896b70a8d4fccab8db18380ce3b50cb4b3c49
baron816 <baron.willeford@gmail.com>
2015-08-12 14:04:57 -0400

    adds resource for public profile

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, except: [:index, :destroy]
+      resources :users, except: [:index, :destroy] do
+        get 'public_profile', on: :member
+      end
     end
   end
 end
