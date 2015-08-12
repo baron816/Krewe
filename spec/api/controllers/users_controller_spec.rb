9775bffff779ad2ceb4ae9c2fc7772fd8b60572b
baron816 <baron.willeford@gmail.com>
2015-08-12 12:30:41 -0400

    adds post tests;

--- spec/api/controllers/users_controller_spec.rb
+++ spec/api/controllers/users_controller_spec.rb
@@ -12,5 +12,26 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it "returns the information about a user" do
       expect(json_response[:email]).to eq(@user.email)
     end
+
+    it "responds with HTTP status 200" do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST #create" do
+    context "when it's successfully created" do
+      before do
+        @user_attributes = attributes_for(:user_home)
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json resprentation for the user record" do
+        expect(json_response[:email]).to eq(@user_attributes[:email])
+      end
+
+      it "responds with HTTP status 201" do
+        expect(response).to have_http_status(201)
+      end
+    end
   end
 end
