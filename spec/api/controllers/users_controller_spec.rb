82d5ef0bfeb68cd3300d68b3b4d6e09f789e18b2
baron816 <baron.willeford@gmail.com>
2015-08-12 12:36:20 -0400

    adds test if create fails

--- spec/api/controllers/users_controller_spec.rb
+++ spec/api/controllers/users_controller_spec.rb
@@ -33,5 +33,21 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(response).to have_http_status(201)
       end
     end
+
+    context "when it's not created" do
+      before do
+        @invalid_user_attributes = attributes_for(:user_home)
+        @invalid_user_attributes[:password] = 'zxcvbnnn'
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders and errors json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        expect(json_response[:errors][:password_confirmation]).to include "doesn't match Password"
+      end
+    end
   end
 end
