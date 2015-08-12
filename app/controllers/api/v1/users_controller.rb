4f390c79c318ea61a73efba1b101c924f0269940
baron816 <baron.willeford@gmail.com>
2015-08-12 12:30:51 -0400

    add create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+  respond_to :json
+
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+  def user_params
+		params.require(:user).permit(:name, :email, :address, :password, :password_confirmation, :street, :city, :state, :category, :latitude, :longitude)
+	end
 end
