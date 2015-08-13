325904f0a98d07d6ed9daf4efad64f7e0b37a264
baron816 <baron.willeford@gmail.com>
2015-08-13 11:25:28 -0400

    adds tests for unsuccessful create

--- spec/api/controllers/activities_controller_spec.rb
+++ spec/api/controllers/activities_controller_spec.rb
@@ -8,15 +8,41 @@ RSpec.describe Api::V1::ActivitiesController, type: :controller do
   end
 
   describe "POST #create" do
-    before do
-      @activity_attributes = attributes_for(:activity)
-      @activity_attributes[:group_id] = @group.id
-      @activity_attributes[:proposer_id] = @user.id
-      post :create, { activity: @activity_attributes }
+    context "when successful" do
+      before do
+        @activity_attributes = attributes_for(:activity)
+        @activity_attributes[:group_id] = @group.id
+        @activity_attributes[:proposer_id] = @user.id
+        post :create, { activity: @activity_attributes }
+      end
+
+      it "is successful" do
+        expect(response).to have_http_status(201)
+      end
+
+      it "returns the created activity" do
+        expect(json_response[:location]).to eq("South Street Seaport")
+      end
     end
 
-    it "is successful" do
-      expect(response).to have_http_status(201)
+    context "when not successful" do
+      before do
+        @activity_attributes = attributes_for(:activity)
+        @activity_attributes[:location] = nil
+        post :create, { activity: @activity_attributes}
+      end
+
+      it "is not succsssful" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "has an errors json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "indicates the error" do
+        expect(json_response[:errors][:location]).to eq(["can't be blank"])
+      end
     end
   end
 end
