2ef3882d1d48328a05022bb6c2541f81b055fc7f
baron816 <baron.willeford@gmail.com>
2015-08-13 15:54:49 -0400

    adds sessions api controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SessionsController < ApplicationController
+  include Authenticable
+
+  def create
+    user_password = params[:session][:password]
+    user_email = params[:session][:email]
+    user = User.find_by(email: user_email.downcase).try(:authenticate, user_password)
+
+    if user
+      log_in(user)
+      user.generate_token(:auth_token)
+      user.save
+      render json: user, status: 200
+    else
+      render json: { errors: "Invalid email or password" }, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find_by(auth_token: params[:id])
+    user.generate_token(:auth_token)
+    user.save
+    head 204
+  end
+end
