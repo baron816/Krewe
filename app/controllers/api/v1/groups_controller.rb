f1ede7d91afcbb397f91af55deb4f5adb492fff0
baron816 <baron.willeford@gmail.com>
2015-08-13 10:51:10 -0400

    adds drop_user method

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::GroupsController < ApplicationController
   respond_to :json
+  before_action :set_group
 
   def show
-    render json: Group.find(params[:id])
+    render json: @group
+  end
+
+  def drop_user
+    @group.drop_user(current_user)
+    head 204
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
