e35d5de40c92c9d6975516466428f663bacefe7f
baron816 <baron.willeford@gmail.com>
2015-08-13 15:53:15 -0400

    simplifies password

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 	factory :user do
 		sequence(:name) { Faker::Name.name }
 		sequence(:email) { Faker::Internet.email }
-		password = Faker::Internet.password
+		password = '123456'
 		sequence(:password) { password }
 		sequence(:password_confirmation) { password }
 
