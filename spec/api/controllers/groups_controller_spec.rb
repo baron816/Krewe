e05a693ec0e7ca35b079b41e96f731a4ea92c357
baron816 <baron.willeford@gmail.com>
2015-08-13 10:43:12 -0400

    adds group controller spec for api

---
+++ spec/api/controllers/groups_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::GroupsController, type: :controller do
+  before do
+    user = create(:user_home)
+    cookies[:auth_token] = user.auth_token
+    @group = user.groups.first
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, id: @group
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the correct json data" do
+      expect(json_response[:name]).to eq(@group.name)
+    end
+  end
+end
