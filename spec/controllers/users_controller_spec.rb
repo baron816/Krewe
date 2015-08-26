f2cc8a9c43435f532d2f2c2d6b2548bf48ed074c
baron816 <baron.willeford@gmail.com>
2015-08-26 15:16:44 -0400

    removes add group test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -88,10 +88,4 @@ describe UsersController do
     	expect(response).to render_template(:new)
     end
   end
-
-  describe "POST #add_group" do
-    it "adds a group" do
-    	expect { post :add_group, id: @user.id }.to change(@user.groups, :count).by(1)
-    end
-  end
 end
