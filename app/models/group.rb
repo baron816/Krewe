966991dae572aed6e628d427e605af869f805182
baron816 <baron.willeford@gmail.com>
2015-08-26 11:07:21 -0400

    adds degree_groups scope and search looks for first degree groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -16,9 +16,10 @@ class Group < ActiveRecord::Base
 	scope :category_groups, ->(category) { where(category: category) }
 	scope :excluded_users, ->(friend_ids) { where.not(id: user_friend_group_ids(friend_ids)) }
 	scope :non_former_groups, ->(group_ids) { where.not(id: group_ids) }
+	scope :degree_groups, ->(degree) { where(degree: degree) }
 
 	def self.search(params)
-		self.open_groups.category_groups(params[:category]).excluded_users(params[:friend_ids]).near([params[:latitude], params[:longitude]], 0.5).non_former_groups(params[:group_ids])[0]
+		self.open_groups.category_groups(params[:category]).excluded_users(params[:friend_ids]).degree_groups(1).near([params[:latitude], params[:longitude]], 0.5).non_former_groups(params[:group_ids])[0]
 	end
 
 	def future_activities
