cde5ace77c1df4a10aa12c7358a1fb576e5e4009
baron816 <baron.willeford@gmail.com>
2015-09-01 11:38:41 -0400

    adds newsletter controller

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,18 @@
+class NewslettersController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:newsletter][:email]
+    begin
+      RestClient.post("https://api:key-#{ENV['MAILGUN_API_KEY']}" \
+                  "@api.mailgun.net/v3/lists/thelist@kreweapp.com/members",
+                  subscribed: true,
+                  address: email)
+
+      redirect_to root_path, notice: "Awesome, #{email} is now signed up for the Krewe newsletter."
+    rescue
+      redirect_to root_path, notice: "I think you signed up already."
+    end
+  end
+end
