2c3073a6e47da19711b14941809edbedb8441d64
baron816 <baron.willeford@gmail.com>
2015-09-02 10:35:50 -0400

    fixed some tests and added tests for had_attended_activities

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -6,7 +6,7 @@ describe Activity do
 		@user2 = create(:user_wtc)
     @user3 = create(:user_121)
 
-    @activity = Activity.create(group_id: Group.first, proposer_id: @user1, appointment: (Time.now + 1.week), plan: "do stuff" )
+    @activity = Activity.create(group_id: Group.first.id, proposer_id: @user1.id, appointment: (Time.now + 1.week), plan: "do stuff", location: "Seaport" )
   end
 
   it "created @activity" do
@@ -46,6 +46,26 @@ describe Activity do
       @activity.users.delete(@user3)
       @activity.check_attendance
       expect(@activity.well_attended).to eq(false)
+    end
+  end
+
+  describe "#had_attended_activities?" do
+    before do
+
+      @activity2 = Activity.create(group_id: Group.first.id, proposer_id: @user1.id, appointment: (Time.now - 1.week), plan: "do things", location: "Seaport" )
+
+      @activity2.users << [@user1, @user2]
+    end
+
+    it "does not have any had_attended_activities" do
+      expect(Activity.had_attended_activities?.size).to eq(0)
+    end
+
+    it "has had_attended_activities once user is added" do
+      @activity2.users << @user3
+      @activity2.check_attendance
+
+      expect(Activity.had_attended_activities?.size).to eq(1)
     end
   end
 
