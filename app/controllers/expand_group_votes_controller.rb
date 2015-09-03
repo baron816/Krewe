ce5e5b8be678d957ca7b621eb7fae468d51fee98
baron816 <baron.willeford@gmail.com>
2015-09-03 11:44:38 -0400

    votes respond to js

--- app/controllers/expand_group_votes_controller.rb
+++ app/controllers/expand_group_votes_controller.rb
@@ -2,14 +2,20 @@ class ExpandGroupVotesController < ApplicationController
   before_action :set_group
 
   def create
-    ExpandGroupVote.create(group_id: @group.id, voter_id: current_user.id)
-    redirect_to @group
+    @expand_vote = ExpandGroupVote.create(group_id: @group.id, voter_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to @group }
+      format.js
+    end
   end
 
   def destroy
     @vote = ExpandGroupVote.find(params[:id])
     @vote.destroy
-    redirect_to @group
+    respond_to do |format|
+      format.html { redirect_to @group }
+      format.js
+    end
   end
 
   private
