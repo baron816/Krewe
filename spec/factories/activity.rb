7ed381f17f81c9c9b8b5d6d6e3bb26715fcb54fc
baron816 <baron.willeford@gmail.com>
2015-09-03 16:46:50 -0400

    adds past and future activity factories

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -1,8 +1,20 @@
 FactoryGirl.define do
 	factory :activity do
 		plan { Faker::Lorem.sentence(3) }
-		location "South Street Seaport"
-		association :proposer, factory: :user_home
-		appointment { Faker::Time.between(DateTime.now, DateTime.now + 8)}
+		location { Faker::Lorem.sentence(3) }
+		longitude { Faker::Address.longitude }
+		latitude { Faker::Address.latitude }
+		group
+
+		factory :activity_future do
+			association :proposer, factory: :user_home
+			appointment { Faker::Time.between(DateTime.now, DateTime.now + 8)}
+		end
+
+		factory :activity_past do
+			association :proposer, factory: :user_home
+			appointment { Faker::Time.between(DateTime.now - 1.month, DateTime.now)}
+			well_attended true
+		end
 	end
 end
