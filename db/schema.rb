c04b7d2bde72a98c378cf84dcaa14f25a117d196
baron816 <baron.willeford@gmail.com>
2015-09-04 10:00:46 -0400

    adds has_expanded to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903161931) do
+ActiveRecord::Schema.define(version: 20150904132653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150903161931) do
     t.datetime "updated_at"
     t.integer  "degree",          default: 1
     t.boolean  "ready_to_expand", default: false
+    t.boolean  "has_expanded",    default: false
   end
 
   create_table "messages", force: :cascade do |t|
