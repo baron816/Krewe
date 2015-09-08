bea6c15f5532e1f3b63cd8481d79f95acee0abd5
baron816 <baron.willeford@gmail.com>
2015-09-08 10:49:20 -0400

    updates schema with indexes and new default for group limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904132653) do
+ActiveRecord::Schema.define(version: 20150908144534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 20150904132653) do
     t.integer  "dropped_group_ids",      default: [],              array: true
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.integer  "group_limit",            default: 3
+    t.integer  "group_limit",            default: 1
   end
 
 end
