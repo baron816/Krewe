6d0023188b64096e32c73ba4f0dd8d2475381cff
baron816 <baron.willeford@gmail.com>
2015-09-10 15:43:55 -0400

    uses expand group class

--- app/controllers/expand_group_votes_controller.rb
+++ app/controllers/expand_group_votes_controller.rb
@@ -3,10 +3,10 @@ class ExpandGroupVotesController < ApplicationController
   before_action :user_logged?
 
   def create
-    @expand_vote = ExpandGroupVote.create(group_id: @group.id, voter_id: current_user.id)
+    @expand_vote = @group.expand_group_votes.create(voter_id: current_user.id)
 
     if @group.voted_to_expand? && @group.ripe_for_expansion?
-      new_group = @group.expand_group
+      new_group = ExpandGroup.new(@group).expand_group
 
       return redirect_to group_path(new_group) if new_group
     end
