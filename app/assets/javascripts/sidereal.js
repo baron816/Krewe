a9fd4cb4c73ab1ecfad44d226173d531e6cfb7fc
baron816 <baron.willeford@gmail.com>
2015-09-11 19:11:09 -0400

    no longer validates input in js

--- app/assets/javascripts/sidereal.js
+++ app/assets/javascripts/sidereal.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	$('.message-box').on("keyup", function(){
-		if ($(this).val().length < 3) {
-			$('#new-message-submit').addClass('disabled')
-		} else {
-			$('#new-message-submit').removeClass('disabled')
-		};
-	})
-
 	$('#datetimepicker').datetimepicker({
 		format: 'YYYY-MM-DD hh:mm A'
 	});
