d1c6b8beea8e2c9e6515402f3f8fdd73adee4990
baron816 <baron.willeford@gmail.com>
2015-09-14 22:37:26 -0400

    adds suggested places

--- app/assets/javascripts/google_places.js
+++ app/assets/javascripts/google_places.js
@@ -53,6 +53,7 @@ function markPlaces(mapOptions, map, infowindow) {
 	var request = {
 		location: mapOptions.center,
 		types: ['bar'],
+    maxPriceLevel: 2,
     radius: '800',
 		rankBy: google.maps.places.RankBy.DISTANCE
 	}
@@ -63,18 +64,32 @@ function markPlaces(mapOptions, map, infowindow) {
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 			for (var i = 0; i < results.length; i++) {
 				createMarker(results[i], map, infowindow, service)
-				console.log(results[i].name);
 			}
+
+      $('#spots').append('<li class="list-group-item">Suggested Places</li>')
+      for (var i = 0; i < 3; i++) {
+        service.getDetails(results[randomNum(results.length)], function (result, status) {
+          $('#spots').append('<li class="list-group-item suggested"><button type="button" class="btn btn-primary name"><span class="name">' + result.name + '</span><span class="hidden longitude" >' + result.geometry.location.lng() + '</span><span class="hidden latitude">' + result.geometry.location.lat() + '</span></button></li>')
+        })
+      }
 		}
 	})
 }
+
+
 
 function createMarker(place, map, infowindow, service) {
 	var placeLoc = place.geometry.location;
 	var marker = new google.maps.Marker({
 		map: map,
-		position: place.geometry.location
+		position: place.geometry.location,
+    icon: {
+    url: 'http://maps.gstatic.com/mapfiles/circle.png',
+    anchor: new google.maps.Point(10, 10),
+    scaledSize: new google.maps.Size(10, 17)
+    }
 	})
+
 
 	google.maps.event.addListener(marker, 'click', function() {
 		service.getDetails(place, function(result, status) {
@@ -82,6 +97,7 @@ function createMarker(place, map, infowindow, service) {
 		    console.error(status)
         return;
 		  }
+
       infowindow.setContent(result.name);
   		infowindow.open(map, marker);
   		document.getElementById('activity_location').value = result.name;
@@ -89,4 +105,8 @@ function createMarker(place, map, infowindow, service) {
   		document.getElementById('activity_latitude').value = result.geometry.location.lat()
 		})
 	})
+}
+
+function randomNum(limit) {
+  return Math.floor((Math.random() * limit) + 1)
 }
