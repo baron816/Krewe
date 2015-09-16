4fa6b54d784d12e872190865741b27339c344ac3
baron816 <baron.willeford@gmail.com>
2015-09-16 11:06:16 -0400

    uses gmaps autocomplete to fill address and set coordinates

--- app/assets/javascripts/sidereal.js
+++ app/assets/javascripts/sidereal.js
@@ -3,44 +3,42 @@ $(document).ready(function(){
 		format: 'YYYY-MM-DD hh:mm A'
 	});
 
-	$('#address-info').change( function () {
-		codeAddress()
+	$("#user_category").select2({
+		placeholder: "Select a category that describes you best"
 	});
-
-	$("#user_state").select2({
-		placeholder: "Select a state"
-	});
-
-	$("#user_category").select2();
 
 	resize_window('.messages')
 
 	resize_window('#map')
 
 	$('.messages').scrollTop($('.messages').prop("scrollHeight"));
+
 })
 
 $(window).resize(function() {
 	resize_window('.messages')
 })
 
+var autocomplete;
+
+function initAutoComplete() {
+	autocomplete = new google.maps.places.Autocomplete(
+		/** @type {!HTMLInputElement} */(document.getElementById('user_address')),
+      {types: ['geocode']});
+
+	autocomplete.addListener('place_changed', setCoordinates)
+}
+
+function setCoordinates() {
+	var place = autocomplete.getPlace();
+	var geo = place.geometry.location
+
+	$('#lat').val(geo.lat())
+	$('#lng').val(geo.lng())
+}
+
 function resize_window(div) {
 	var window_height = $(window).height();
 	var content_height = window_height * .25;
 	$(div).height(content_height);
-}
-
-function codeAddress() {
-	geocoder = new google.maps.Geocoder()
-	var address = $('#user_street').val() + ", " + $('#user_city').val() + ", " + $('#user_state').val();
-	geocoder.geocode( { 'address': address}, function (results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			var lat = results[0].geometry.location.lat()
-			var lng = results[0].geometry.location.lng()
-			$('#lat').val(lat)
-			$('#lng').val(lng)
-		} else {
-			console.log("error" + status);
-		}
-	})
 }
