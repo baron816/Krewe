c5afef661cb0088bbc243aea4f0f9e1591da34e5
baron816 <baron.willeford@gmail.com>
2015-09-17 10:43:26 -0400

    join notifications have join type

--- app/models/group.rb
+++ app/models/group.rb
@@ -76,7 +76,7 @@ class Group < ActiveRecord::Base
 
 	def join_group_notifications(new_user)
 		users.each do |user|
-			self.notifications.create(user: user, poster: new_user, notification_type: self.class.name) unless user == new_user
+			self.notifications.create(user: user, poster: new_user, notification_type: "Join") unless user == new_user
 			# needs testing -- uncomment when email set up
 			# GroupMailer.join_group({user: user, group: self, poster: new_user})
 		end
