baa3d216231f49d2664192e1e5330b76a5dc5f30
baron816 <baron.willeford@gmail.com>
2015-09-18 09:30:14 -0400

    adds facade for activity show

---
+++ app/facades/activity_show.rb
@@ -0,0 +1,16 @@
+class ActivityShow
+  attr_reader :activity, :user
+
+  def initialize(activity, user)
+    @activity = activity
+    @user = user
+  end
+
+  delegate :group, :plan, :latitude, :longitude, :appointment, :location, :proposed_by?, :users, :messages, :group_includes_user?, :user_going?, to: :activity
+  delegate :name, to: :group, prefix: true
+
+  def new_message
+    Message.new(messageable_id: activity.id, poster_id: user, messageable_type: activity.class)
+  end
+
+end
