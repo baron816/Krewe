e0f0c3feebdf656019a761fc607e8c41204b048a
baron816 <baron.willeford@gmail.com>
2015-09-21 12:37:00 -0400

    adds user message alert method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,10 @@ class UserMailer < ApplicationMailer
 
     mail to: user.email, subject: "#{@message.poster_name} mentioned you in a post"
   end
+
+  def user_message_alert(message)
+    @message = message
+
+    mail to: @message.messageable.email, subject: "#{@message.poster_name} sent you a message"
+  end
 end
