1d6cc428c39f8944d8cb80c3a47f6860289e3a82
baron816 <baron.willeford@gmail.com>
2015-09-21 11:12:22 -0400

    adds methods for sending mention alerts

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,7 @@ class Message < ActiveRecord::Base
 
 	validates :content, presence: true, length: { minimum: 2 }
 
-	after_create :send_notifications
+	after_create :send_notifications, :send_mention_email_alerts
 
 	scope :users_messages, -> { where(messageable_type: "User")  }
 	scope :poster_messages,  -> (poster){ where(poster: poster)  }
@@ -25,6 +25,21 @@ class Message < ActiveRecord::Base
 		simple_format
 	end
 
+	private
+	def mentioned_user_slugs
+	  content.scan(/\b(?<=data-name=\")[^"]+(?=\")/)
+	end
+
+	def mentioned_users
+	  User.where(slug: mentioned_user_slugs)
+	end
+
+	def send_mention_email_alerts
+	  mentioned_users.each do |user|
+	    UserMailer.mention_alert(self, user).deliver_now
+	  end
+	end
+
 	def send_notifications
 		case messageable_type
 		when 'Group', 'Activity'
@@ -36,7 +51,6 @@ class Message < ActiveRecord::Base
 		end
 	end
 
-	private
 	def create_notification(user)
 	  self.notifications.create(user: user, poster: self.poster, notification_type: "#{messageable_type}Message")
 	end
