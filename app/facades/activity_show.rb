0e6d3ed8f11369a7b7885efa993472e8597a0e57
baron816 <baron.willeford@gmail.com>
2015-10-06 16:34:16 -0400

    messages include poster

--- app/facades/activity_show.rb
+++ app/facades/activity_show.rb
@@ -6,11 +6,15 @@ class ActivityShow
     @user = user
   end
 
-  delegate :group, :plan, :latitude, :longitude, :appointment, :location, :proposed_by?, :users, :messages, :group_includes_user?, :user_going?, to: :activity
+  delegate :group, :plan, :latitude, :longitude, :appointment, :location, :proposed_by?, :users, :group_includes_user?, :user_going?, to: :activity
   delegate :name, to: :group, prefix: true
 
   def new_message
     Message.new(messageable_id: activity.id, poster_id: user, messageable_type: activity.class)
+  end
+
+  def messages
+    @messages ||= activity.messages.includes(:poster)
   end
 
 end
