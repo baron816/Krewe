4dd3338de52e2e9243ed41e3c50e44fc9806a123
baron816 <baron.willeford@gmail.com>
2015-10-12 15:27:01 -0400

    adds migration for survey

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916220159) do
+ActiveRecord::Schema.define(version: 20151012135733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,13 @@ ActiveRecord::Schema.define(version: 20150916220159) do
   end
 
   add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
+
+  create_table "surveys", force: :cascade do |t|
+    t.string   "email"
+    t.string   "reasons",    array: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "user_activities", force: :cascade do |t|
     t.integer "activity_id"
