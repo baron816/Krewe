67dce9199f512e948b52fc34c254ec481ac9f4b5
baron816 <baron.willeford@gmail.com>
2015-10-14 17:41:07 -0400

    adds load_messageable method for finding and setting poly associated object

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 	before_action :user_logged?
 
 	def create
-		@message = Message.new(message_params)
+		@message = load_messageable.messages.new(message_params)
 
 		@message.save!
 
@@ -14,6 +14,16 @@ class MessagesController < ApplicationController
 
 	private
 	def message_params
-		params.require(:message).permit(:content, :messageable_id, :messageable_type, :poster_id)
+		params.require(:message).permit(:content).merge(poster_id: current_user.id)
+	end
+
+	def load_messageable
+	  if params[:group_id]
+	  	@messageable = Group.friendly.find(params[:group_id])
+		elsif params[:user_id]
+			@messageable = User.friendly.find(params[:user_id])
+	  elsif params[:activity_id]
+			@messageable = Activity.find(params[:activity_id])
+		end
 	end
 end
