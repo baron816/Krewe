02aeda832de61ae1af5a5e38c3fb385edbc4811c
baron816 <baron.willeford@gmail.com>
2015-10-14 18:17:00 -0400

    checks if user is in group first

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,16 +3,23 @@ class GroupsController < ApplicationController
 		group = Group.friendly.find(params[:id])
 		@group_show = GroupShow.new(group, current_user, params[:page])
 
-		redirect_to user_path(current_user) unless @group_show.includes_user?(current_user)
-		current_user.dismiss_group_notifications_from_group(group)
+		if @group_show.includes_user?(current_user)
+			current_user.dismiss_group_notifications_from_group(group)
+		else
+			redirect_to user_path(current_user)
+		end
 	end
 
 	def drop_user
 		@group = Group.includes(:users).friendly.find(params[:id])
-		@group.drop_user(current_user)
-		if @group.primary_group?
-			new_group = current_user.find_or_create_group
-			redirect_to group_path(new_group)
+		if @group.includes_user?(current_user)
+			@group.drop_user(current_user)
+			if @group.primary_group?
+				new_group = current_user.find_or_create_group
+				redirect_to group_path(new_group)
+			else
+				redirect_to user_path(current_user)
+			end
 		else
 			redirect_to user_path(current_user)
 		end
