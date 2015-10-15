ab1d243eefc201cb7ec6edd87d83d226ccc26461
baron816 <baron.willeford@gmail.com>
2015-10-15 18:30:19 -0400

    adds topic controller

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,12 @@
+class TopicsController < ApplicationController
+  before_action :get_topic
+
+  def show
+    @topic = TopicShow.new(@topic, params[:page])
+  end
+
+  private
+  def get_topic
+    @topic = Topic.find(params[:id])
+  end
+end
