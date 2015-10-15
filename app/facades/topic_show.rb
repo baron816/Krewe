8d93ea1092cb06e8bb6c9b3a8b1fec1f0fb11ddd
baron816 <baron.willeford@gmail.com>
2015-10-15 18:31:35 -0400

    adds topic show

---
+++ app/facades/topic_show.rb
@@ -0,0 +1,19 @@
+class TopicShow
+  attr_reader :this_topic, :page, :per_page
+  def initialize(topic, page)
+    @this_topic = topic
+    @page = page
+    @per_page = 5
+  end
+
+  delegate :any?, to: :messages, prefix: true
+  delegate :names_data, to: :this_topic
+
+  def messages
+    @messages ||= this_topic.messages.includes(:poster).paginate(page: page, per_page: per_page).order(created_at: :desc)
+  end
+
+  def new_message
+    @message ||= Message.new
+  end
+end
