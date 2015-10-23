9ba4c5c8ea845cc1a8cad8ecd6a9f6044a87a2b8
baron816 <baron.willeford@gmail.com>
2015-10-23 12:17:01 -0400

    delegates next_page to messages and only needs messages method

--- app/facades/topic_show.rb
+++ app/facades/topic_show.rb
@@ -9,20 +9,13 @@ class TopicShow
   delegate :any?, to: :messages, prefix: true
   delegate :names_data, :id, to: :this_topic
   delegate :count, to: :found_messages, prefix: true
+  delegate :next_page, to: :messages
 
   def messages
-    @messages ||= found_messages.page(page).per(per_page).order(created_at: :desc)
-  end
-
-  def found_messages
-    @found_messages ||= this_topic.messages.includes(:poster)
+    @messages ||= this_topic.messages.includes(:poster).page(page).per(per_page).order(created_at: :desc)
   end
 
   def new_message
     @message ||= Message.new
-  end
-
-  def multiple_pages?
-    found_messages_count > per_page
   end
 end
