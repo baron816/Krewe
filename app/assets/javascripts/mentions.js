e208715e9fdbc3a69bf5f31fb56f37ea718b5f95
baron816 <baron.willeford@gmail.com>
2015-10-28 14:39:19 -0400

    no longer provides link to mentioned

--- app/assets/javascripts/mentions.js
+++ app/assets/javascripts/mentions.js
@@ -1,9 +1,9 @@
 function mentioning(dataHash) {
   var data = dataHash;
-  var at_config = {
+  at_config = {
     at: "@",
     displayTpl: "<li>${name} <small>${full_name}</small></li>",
-    insertTpl: '<a href="/users/${slug}/public_profile" data-name="${slug}">@${name}</a>',
+    insertTpl: '@${name}',
     data: data
   }
 
