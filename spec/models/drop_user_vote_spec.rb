b0513fa2ba415cbcb2229357bd929ff73f178246
baron816 <baron.willeford@gmail.com>
2015-10-30 13:44:29 -0400

    uses let

--- spec/models/drop_user_vote_spec.rb
+++ spec/models/drop_user_vote_spec.rb
@@ -1,51 +1,49 @@
 require "rails_helper"
 
 describe DropUserVote do
-  before do
-    @user1 = create(:user_home)
-    @user2 = create(:user_wtc)
-    @user3 = create(:user_121)
-    @user4 = create(:user_dbc)
-  end
+  let!(:user1) { create(:user_home) }
+  let!(:user2) { create(:user_wtc) }
+  let!(:user3) { create(:user_121) }
+  let!(:user4) { create(:user_dbc) }
   let(:group) { Group.first }
 
   it "@user doesn't have any drop_user_votes" do
-  	expect(@user1.group_drop_votes_count(group)).to eql(0)
+  	expect(user1.group_drop_votes_count(group)).to eql(0)
   end
 
   describe "User#can_vote?" do
     before do
-      DropUserVote.create(voter: @user1, user: @user2, group: group)
+      DropUserVote.create(voter: user1, user: user2, group: group)
     end
 
     it "doesn't let user vote for itself" do
-    	expect(@user1.can_vote?(@user1)).to eql(false)
+    	expect(user1.can_vote?(user1)).to eql(false)
     end
 
     it "does let user vote for another voter" do
-    	expect(@user1.can_vote?(@user3)).to eql(true)
+    	expect(user1.can_vote?(user3)).to eql(true)
     end
 
     it "does not let user vote for another user twice" do
-    	expect(@user1.can_vote?(@user2)).to eql(false)
+    	expect(user1.can_vote?(user2)).to eql(false)
     end
   end
 
   describe "Group#kick_user" do
     before do
-      DropUserVote.create(voter: @user1, user: @user2, group: group)
-      DropUserVote.create(voter: @user3, user: @user2, group: group)
+      DropUserVote.create(voter: user1, user: user2, group: group)
+      DropUserVote.create(voter: user3, user: user2, group: group)
     end
 
     it "doesn't kick user after two votes" do
-    	group.kick_user(@user2)
-    	expect(group.users).to include(@user2)
+    	group.kick_user(user2)
+    	expect(group.users).to include(user2)
     end
 
     it "kicks user after third vote" do
-    	DropUserVote.create(voter: @user4, user: @user2, group: group)
-    	group.kick_user(@user2)
-    	expect(group.users).not_to include(@user2)
+    	DropUserVote.create(voter: user4, user: user2, group: group)
+    	group.kick_user(user2)
+    	expect(group.users).not_to include(user2)
     end
   end
 end
