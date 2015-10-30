091a5bfd10471b2d6875e81ccf4b91fdd04df68c
baron816 <baron.willeford@gmail.com>
2015-10-30 17:50:25 -0400

    removes delayed jobs

--- app/jobs/send_mention_email_job_job.rb
+++
@@ -1,7 +0,0 @@
-class SendMentionEmailJobJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(message, user)
-    UserMailer.mention_alert(message, user).deliver_later
-  end
-end
--- app/jobs/send_personal_email_job.rb
+++
@@ -1,7 +0,0 @@
-class SendPersonalEmailJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(message)
-    UserMailer.user_message_alert(message).deliver_later
-  end
-end
