15610f3fa4cef29bd6601c3c818d6037558ded36
baron816 <baron.willeford@gmail.com>
2015-10-30 13:48:38 -0400

    uses let and messageable is a topic

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,26 +1,23 @@
 require "rails_helper"
 
 describe "Message" do
+  let!(:user) { create(:user_home) }
+  let!(:message) { Message.create(messageable: user.groups.first.topics.first, poster: user, content: Faker::Lorem.sentence(5, true, 8)) }
+
   before do
-    @user = create(:user_home)
     create(:user_wtc)
     create(:user_121)
-    @message = Message.create(messageable: @user.groups.first, poster: @user, content: Faker::Lorem.sentence(5, true, 8))
   end
 
   it "has string content" do
-    expect(@message.content).to be_a(String)
+    expect(message.content).to be_a(String)
   end
 
   it "has a user" do
-  	expect(@message.poster).to be_a(User)
+  	expect(message.poster).to be_a(User)
   end
 
   it "has a group" do
-  	expect(@message.messageable).to be_a(Group)
-  end
-
-  it "has a group with a latitude" do
-    expect(@message.messageable.latitude).to eql(40.7094706)
+  	expect(message.messageable).to be_a(Topic)
   end
 end
