bb6a9ac812a0d70b1f863b75adb3fec20a233969
baron816 <baron.willeford@gmail.com>
2015-11-04 14:13:22 -0500

    uses instance variable

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,15 +3,14 @@ class UserMailer < ApplicationMailer
   def password_reset(user)
     @user = user
 
-    mail to: user.email, subject: "Reset Krewe Password"
+    mail to: @user.email, subject: "Reset Krewe Password"
   end
 
   def mention_alert(message, user)
-    p "{{{{{{{{{success}}}}}}}}}"
     @user = user
     @message = message
 
-    mail to: user.email, subject: "#{@message.poster_name} mentioned you in a post"
+    mail to: @user.email, subject: "#{@message.poster_name} mentioned you in a post"
   end
 
   def user_message_alert(message)
