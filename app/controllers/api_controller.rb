a7ca272a95608d47183446a6f933fb66fe9cd2c3
baron816 <baron.willeford@gmail.com>
2015-11-05 08:42:43 -0500

    correctly configures api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,10 @@
-class ApiController < ApplicationController
+class ApiController < ActionController::Base
+  protect_from_forgery with: :null_session
   include Authenticable
+
+  before_action :user_logged?, except: [:new, :index, :create, :edit, :update]
+
+  def user_logged?
+    head 403 unless user_signed_in?
+  end
 end
