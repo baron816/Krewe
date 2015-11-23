eba07badc48cf4be613bfb919937763c2e8c7645
baron816 <baron.willeford@gmail.com>
2015-11-23 18:23:18 -0500

    has many notifications

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :group
   has_many :messages, as: :messageable
+  has_many :notifications, as: :notifiable
   delegate :names_data, :users, to: :group
 
   validates_presence_of :name
