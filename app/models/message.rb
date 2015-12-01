ba5ca334937ab0fb55621281be9d90f3b711cfbf
baron816 <baron.willeford@gmail.com>
2015-12-01 15:28:01 -0500

    adds delegations for policy

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,8 @@ class Message < ActiveRecord::Base
 	has_many :notifications, as: :notifiable
 
 	delegate :name, to: :poster, prefix: true
-	delegate :users, :group, to: :messageable, prefix: true
+	delegate :users, :group, :is_friends_with?, to: :messageable, prefix: true
+	delegate :group_includes_user?, to: :messageable
 
 	validates :content, presence: true
 	validates_presence_of :messageable, :messageable_type, :poster
