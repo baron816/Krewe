15cea33bdfe4ec5b89094e9566f9b9a86d90d996
baron816 <baron.willeford@gmail.com>
2015-12-01 16:04:55 -0500

    delegates to group

--- app/models/expand_group_vote.rb
+++ app/models/expand_group_vote.rb
@@ -2,6 +2,8 @@ class ExpandGroupVote < ActiveRecord::Base
   belongs_to :voter, class_name: "User"
   belongs_to :group
 
+  delegate :includes_user?, to: :group, prefix: true
+
   validates_presence_of :voter, :group
 
   def self.user_votes(user)
