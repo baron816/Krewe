4c9bfa259248f843d93c70ccfb8c20cf7bfef395
baron816 <baron.willeford@gmail.com>
2015-12-02 12:04:54 -0500

    adds tests for activity proposal

--- spec/mailers/group_mailer_spec.rb
+++ spec/mailers/group_mailer_spec.rb
@@ -15,7 +15,24 @@ RSpec.describe GroupMailer, :type => :mailer do
       expect(mail.to).to eq([@user.email])
       expect(mail.from).to eq(["no-reply@gokrewe.com"])
     end
-
   end
 
+  describe "activity_proposal" do
+    before do
+      @user = create(:user_home)
+      @activity = create(:activity_future)
+    end
+
+    let(:mail) { GroupMailer.activity_proposal(@user, @activity) }
+
+    it "sends email" do
+      expect{ GroupMailer.delay.activity_proposal(@user, @activity) }.to change(Sidekiq::Extensions::DelayedMailer.jobs, :size).by(1)
+    end
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("#{@activity.proposer.name} proposed an activity: #{@activity.plan}")
+      expect(mail.to).to eq([@user.email])
+      expect(mail.from).to eq(["no-reply@gokrewe.com"])
+    end
+  end
 end
