ffa2a4b27c0f75f8f28d05fedea484d48bc95991
baron816 <baron.willeford@gmail.com>
2015-12-06 17:47:09 -0500

    paginates groups

--- app/facades/admin_dash.rb
+++ app/facades/admin_dash.rb
@@ -1,10 +1,15 @@
 class AdminDash
+  attr_reader :page
+  def initialize(page)
+    @page = page
+  end
+
   def group_count
     Group.count
   end
 
   def groups
-    Group.all
+    @groups ||= Group.all.page(page).per(15)
   end
 
   def users_count
