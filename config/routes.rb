c79e8d1440b0b44e2e800979a6a2d5b5ff06f7c9
baron816 <baron.willeford@gmail.com>
2015-12-09 18:08:23 -0500

    adds route to about

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get "admin_dash" => "home#admin_dash"
   get "faq" => "home#faq"
+  get "about" => "home#about"
   get "privacy_policy" => "home#privacy_policy"
   get "terms_of_service" => "home#terms_of_service"
   get "*any", via: :all, to: "errors#not_found"
