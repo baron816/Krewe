c1a6be8bd27ec2a43ba9f3a3a924faedcfaf848b
baron816 <baron.willeford@gmail.com>
2015-12-09 12:38:33 -0500

    refactors to limit unwanted emails

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -9,20 +9,23 @@ class GroupMailer < ApplicationMailer
     @group = params[:group]
     @poster = params[:poster]
 
-    mail bcc: emails, subject: "#{@poster.name} joined group #{@group.name}"
+    mail bcc: emails("join"), subject: "#{@poster.name} joined group #{@group.name}"
   end
 
-  def activity_proposal(params = {})
-    @group = params[:group]
-    @activity = params[:activity]
+  def activity_proposal(activity)
+    @activity = activity
+    @group = @activity.group
     @poster = @activity.proposer
 
-    mail bcc: emails, subject: "#{@poster.name} proposed an activity: #{@activity.plan}"
+    mail bcc: emails("proposal"), subject: "#{@poster.name} proposed an activity: #{@activity.plan}"
   end
 
   private
 
-  def emails
-    @group.users.pluck(:email) - [@poster.email]
+  def emails(type)
+    @group.users.inject([]) do |results, user|
+      results << user.email if user != @poster && user.send_notification?(type)
+      results
+    end
   end
 end
