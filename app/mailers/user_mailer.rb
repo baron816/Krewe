2081f7176afb214b5c76d187b08103d6695a51cd
baron816 <baron.willeford@gmail.com>
2015-12-09 15:11:50 -0500

    moves mention_alert to group mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,12 +12,7 @@ class UserMailer < ApplicationMailer
     mail to: @user.email, subject: "Reset Krewe Password"
   end
 
-  def mention_alert(message, user)
-    @user = user
-    @message = message
 
-    mail to: @user.email, subject: "#{@message.poster_name} mentioned you in a post"
-  end
 
   def user_message_alert(message)
     @message = message
