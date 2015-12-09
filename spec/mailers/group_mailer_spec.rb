ff556b3d99baa64c6647127b0e4a3f210f447e49
baron816 <baron.willeford@gmail.com>
2015-12-09 12:41:58 -0500

    refactors and adds tests

--- spec/mailers/group_mailer_spec.rb
+++ spec/mailers/group_mailer_spec.rb
@@ -1,39 +1,40 @@
 require "rails_helper"
 
 RSpec.describe GroupMailer, :type => :mailer do
-  describe "join_group" do
-    before do
-      @user = create(:user_home)
-      @user2 = create(:user_home)
-      @poster = create(:user_wtc)
-      @group = @user.groups.first
-    end
+  let!(:user) { create(:user_home) }
+  let!(:user2) { create(:user_dbc) }
+  let!(:user3) { create(:user_121) }
+  let!(:poster) { create(:user_wtc) }
+  let!(:group) { Group.first }
 
-    let(:mail) { GroupMailer.join_group({group: @group, poster: @poster}) }
+  describe "join_group" do
+
+    let(:mail) { GroupMailer.join_group({group: group, poster: poster}) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("#{@poster.name} joined group #{@group.name}")
-      expect(mail.bcc).to include(@user.email, @user2.email)
+      expect(mail.subject).to eq("#{poster.name} joined group #{group.name}")
+      expect(mail.bcc).to include(user.email, user2.email)
+      expect(mail.bcc).to_not include(user3.email, poster.email)
       expect(mail.from).to eq(["no-reply@gokrewe.com"])
     end
   end
 
   describe "activity_proposal" do
+    let!(:activity) { create(:activity_future) }
     before do
-      @user = create(:user_home)
-      @activity = create(:activity_future)
-      @group = @user.groups.first
+      group.activities << activity
     end
 
-    let(:mail) { GroupMailer.activity_proposal({activity: @activity, group: @group}) }
+    let(:mail) { GroupMailer.activity_proposal(activity) }
 
     it "sends email" do
-      expect{ GroupMailer.delay.activity_proposal({activity: @activity, group: @group}) }.to change(Sidekiq::Extensions::DelayedMailer.jobs, :size).by(1)
+      expect{ GroupMailer.delay.activity_proposal(activity) }.to change(Sidekiq::Extensions::DelayedMailer.jobs, :size).by(1)
     end
 
     it "renders the headers" do
-      expect(mail.subject).to eq("#{@activity.proposer.name} proposed an activity: #{@activity.plan}")
-      expect(mail.bcc).to eq([@user.email])
+      expect(mail.subject).to eq("#{activity.proposer.name} proposed an activity: #{activity.plan}")
+      expect(mail.bcc).to include(user.email, user2.email)
+      expect(mail.bcc).to_not include(user3.email, activity.proposer.email)
       expect(mail.from).to eq(["no-reply@gokrewe.com"])
     end
   end
