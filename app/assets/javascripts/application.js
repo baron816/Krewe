55c6e22c87e3209c86ea299c04cff66a1b526e55
baron816 <baron.willeford@gmail.com>
2016-01-02 16:22:28 -0500

    adds turbolinks back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require browser_timezone_rails/set_time_zone
 //= require jquery_ujs
 //= require materialize-sprockets
+//= require jquery.turbolinks
 //= require moment
 //= require underscore
 //= require_tree
