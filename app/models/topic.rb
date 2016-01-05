648cd3667eb85fb89aead6823f4aeba00ffde0ee
baron816 <baron.willeford@gmail.com>
2016-01-05 15:39:57 -0500

    removes names data delegation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   belongs_to :group
   has_many :messages, as: :messageable
   has_many :notifications, as: :notifiable
-  delegate :names_data, :users, to: :group
+  delegate :users, to: :group
   delegate :includes_user?, to: :group, prefix: true
 
   validates :name, presence: true, length: { maximum: 20 }
