9723109e9501f985bb4e79de913122ed6d9253b4
baron816 <baron.willeford@gmail.com>
2016-01-12 14:41:28 -0500

    mention data taken from data tag

--- app/assets/javascripts/mentions.js
+++ app/assets/javascripts/mentions.js
@@ -1,12 +1,14 @@
-function mentioning(dataHash) {
-  var data = dataHash;
-  at_config = {
-    at: "@",
-    displayTpl: "<li>${name} <small>${full_name}</small></li>",
-    insertTpl: '<span data-name="${slug}">@${name}</span>',
-    data: data
-  }
+function Mentions() {
+  this.mentioning = function() {
+    var data = $('.messages').data("namesdata");
+    at_config = {
+      at: "@",
+      displayTpl: "<li>${name} <small>${full_name}</small></li>",
+      insertTpl: '<span data-name="${slug}">@${name}</span>',
+      data: data
+    }
 
-  $('.atwho-container').remove()
-  $('.new-message').atwho(at_config);
+    $('.atwho-container').remove()
+    $('.new-message').atwho(at_config);
+  };
 }
