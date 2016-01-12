421d99eb95b094ab3a62c1cb0bb2bdb3d839b0f3
baron816 <baron.willeford@gmail.com>
2016-01-12 14:39:15 -0500

    adds file dedicated to listening to default js calls

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  new Formatter().setInitDefaults();
+
+  new Mentions().mentioning();
+
+	$("#map-hider").click(function () {
+		$('#map-activity-canvas').toggle();
+	})
+
+	if ($('body').data("notice")) {
+		swal($('body').data("notice"));
+	}
+
+  if ($(".activities.show").length) {
+    mapActivityLocation();
+  }
+
+  if ($(".activities.edit").length || $(".activities.new").length) {
+    new Activity().activitySetup();
+  }
+
+  if ($('.users.edit').length || $('.users.new').length) {
+    initAutoComplete();
+  }
+
+  $('#new_user').submit(function(event){
+    validateCheckbox(event);
+  })
+
+  setMessageBox();
+})
