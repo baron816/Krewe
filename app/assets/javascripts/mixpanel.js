1b1869e32f3b4a8f5f34ca08a692d8934151ce65
baron816 <baron.willeford@gmail.com>
2016-01-12 14:36:46 -0500

    calls mixpanel on correct page

--- app/assets/javascripts/mixpanel.js
+++ app/assets/javascripts/mixpanel.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
   $('.new_user').submit(function () {
     mixpanel.track("Finished Signup")
   })
+
+  if ($(".home.landing").length) {
+    mixpanel.track("Viewed landing")
+  }
+
+  if ($(".home.faq").length) {
+    mixpanel.track("Viewed FAQ")
+  }
 })
