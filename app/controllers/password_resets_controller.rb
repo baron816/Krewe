4631cd87593356cc6499713d7aa570a3df29cf79
baron816 <baron.willeford@gmail.com>
2016-01-21 15:23:55 -0500

    checks for user by password reset token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,5 @@
 class PasswordResetsController < ApplicationController
 	before_action :set_user, only: [:edit, :update]
-	before_action :check_expiration, only: [:edit, :update]
 
 	def new
 	end
@@ -9,22 +8,24 @@ class PasswordResetsController < ApplicationController
 		email = params[:password_reset][:email]
 		@user = User.find_by(email: email.downcase)
 
-		@user.send_password_reset if @user
+		user_password_reset.send_password_reset if @user
 
 		redirect_to root_path, notice: "An email was just sent to #{email} to reset your password. It will expire in one hour."
 	end
 
 	def edit
-
+		return redirect_to get_started_path unless @user
+		check_expiration
 	end
 
 	def update
+		check_expiration
 		if params[:user][:password].empty? || params[:user][:password_confirmation].empty?
 			flash.now[:danger] = "Password can't be empty"
 			render :edit
 		elsif @user.update(user_params)
 			log_in(@user)
-			redirect_to @user
+			redirect_to root_path
 		else
 			render :edit
 		end
@@ -32,7 +33,7 @@ class PasswordResetsController < ApplicationController
 
 	private
 	def set_user
-		@user = User.find_by(email: params[:email])
+		@user = User.find_by(password_reset_token: params[:id])
 	end
 
 	def user_params
@@ -40,9 +41,13 @@ class PasswordResetsController < ApplicationController
 	end
 
 	def check_expiration
-		if @user.password_reset_expired?
-			flash[:danger] = "Password reset has expired."
+		if user_password_reset.password_reset_expired?
+			flash[:danger] = "Password reset has expired. Please enter your email again."
 			redirect_to new_password_reset_url
 		end
+	end
+
+	def user_password_reset
+	  @user_password_reset ||= UserPasswordReset.new(@user)
 	end
 end
