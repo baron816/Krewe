642e65e78e0610991e057dc3657756ed776f317c
baron816 <baron.willeford@gmail.com>
2016-01-21 10:34:17 -0500

    adds topics controller spec

---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe TopicsController do
+  let(:user) { create(:user_home) }
+  let(:group) { Group.first }
+  let(:topic) { group.topics.first }
+  before do
+    cookies.signed[:auth_token] = user.auth_token
+  end
+
+  describe "#GET show" do
+    before do
+      get :show, id: topic
+    end
+
+    it "renders the topic template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "sets a TopicShow instance" do
+      expect(assigns[:topic]).to be_a(TopicShow)
+    end
+  end
+
+  describe "#POST create" do
+    it "creates a new topic" do
+      expect{ post :create, group_id: group, topic: { name: "New Thread"} }.to change(Topic, :count).by(1)
+    end
+
+    it "sets a TopicShow instance" do
+      post :create, group_id: group, topic: { name: "A new Thread"}
+      expect(assigns[:topic]).to be_a(TopicShow)
+    end
+  end
+end
