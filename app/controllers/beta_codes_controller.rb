84031d47d61a940a345d945d0a0cadb994aeab80
baron816 <baron.willeford@gmail.com>
2016-01-21 12:04:24 -0500

    redirects to root if current users and adds a notice if unable to save code

--- app/controllers/beta_codes_controller.rb
+++ app/controllers/beta_codes_controller.rb
@@ -1,6 +1,6 @@
 class BetaCodesController < ApplicationController
   def new
-    return redirect_to current_user if current_user
+    return redirect_to root_path if current_user
 
     @beta_code = BetaCode.new
   end
@@ -8,10 +8,10 @@ class BetaCodesController < ApplicationController
   def create
     @beta_code = BetaCode.new(beta_params)
 
-    if @beta_code.save
+    if @beta_code.save!
       redirect_to get_started_path, notice: "Awesome, we sent you an email. Use that to finish signing up."
     else
-      render :new
+      render :new, notice: "Not a valid email"
     end
   end
 
