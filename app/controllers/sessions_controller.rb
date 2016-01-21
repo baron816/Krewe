46627be698d30081527890d6dd78746fc839f187
baron816 <baron.willeford@gmail.com>
2016-01-21 11:44:28 -0500

    actually passes in remember me value

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 		@user = User.find_by(email: user_params[:email].downcase).try(:authenticate, user_params[:password])
 
 		if @user
-			log_in(@user)
+			log_in(@user, { remember_me: user_params[:remember_me] } )
 			redirect_to root_path
 		else
 			flash[:error] = "Email or Password not found"
