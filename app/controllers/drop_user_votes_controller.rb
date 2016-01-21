b50b074ddc9ed6660d841e2195f94de29862e4ef
baron816 <baron.willeford@gmail.com>
2016-01-21 18:08:29 -0500

    adds tests for drop user votets controller

--- app/controllers/drop_user_votes_controller.rb
+++ app/controllers/drop_user_votes_controller.rb
@@ -1,32 +1,25 @@
 class DropUserVotesController < ApplicationController
-	before_action :set_group
-
 	def create
-		@user = User.friendly.find(params[:user_id])
+		group = Group.friendly.find(params[:group_id])
+		user = User.friendly.find(params[:user_id])
 
-		@vote = @group.drop_user_votes.new(user: @user, voter: current_user)
-		authorize! :vote, @vote
-		@vote.save
+		vote = group.drop_user_votes.new(user: user, voter: current_user)
+		authorize! :vote, vote
+		vote.save!
 
-		@group.kick_user(@user)
 		respond_to do |format|
-			format.html { redirect_to @group }
+			format.html { redirect_to group }
 			format.js
 		end
 	end
 
 	def destroy
-		@vote = DropUserVote.find(params[:id])
-		@user = @vote.user
-		@vote.destroy
+		vote = DropUserVote.find(params[:id])
+
+		vote.destroy!
 		respond_to do |format|
-			format.html { redirect_to @group }
+			format.html { redirect_to vote.group }
 			format.js
 		end
-	end
-
-	private
-	def set_group
-		@group = Group.friendly.find(params[:group_id])
 	end
 end
