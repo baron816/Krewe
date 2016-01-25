c9e4eca9c3d0dd8cdf38807442036960b253b937
baron816 <baron.willeford@gmail.com>
2016-01-25 11:53:01 -0500

    adds new message test

--- spec/facades/user_public_profile_spec.rb
+++ spec/facades/user_public_profile_spec.rb
@@ -3,16 +3,27 @@ require "rails_helper"
 describe UserPublicProfile do
   let(:user1){ create(:user_home) }
   let(:user2){ create(:user_home) }
+  let(:public_profile) { UserPublicProfile.new(user1, user2, 1) }
 
   describe "#personal_messages" do
     before do
       create_list(:message, 4, poster: user1, messageable: user2)
       create_list(:message, 4, poster: user1, messageable: user2)
     end
-    let(:public_profile) { UserPublicProfile.new(user1, user2, 1) }
 
-    it "returns the messages" do
+    it "returns all the messages" do
       expect(public_profile.personal_messages.count).to eq(8)
+    end
+
+    it "only returns 5 messages when no unviewed notifications" do
+      user2.dismiss_personal_notifications_from_user(user1)
+      expect(public_profile.personal_messages.count).to eq(5)
+    end
+  end
+
+  describe "#new_message" do
+    it "initializes a new message" do
+      expect(public_profile.new_message).to be_a(Message)
     end
   end
 end
