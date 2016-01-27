f2a2b92001fe431ddde35ead83ca6511538bdbe6
baron816 <baron.willeford@gmail.com>
2016-01-27 11:51:33 -0500

    created handled by sessions, update completes signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :join_group, :destroy]
+	before_action :set_user, only: [:edit, :update, :join_group, :destroy, :new]
 	before_action :authorize_read, only: [:update, :edit, :destroy]
 
 	def show
 		@user_show = current_user
-		return redirect_to get_started_path unless @user_show
+		return redirect_to get_started_path unless current_user
+		return redirect_to new_user_path unless current_user.sign_up_complete?
 		authorize! :read, @user_show
 	end
 
@@ -21,33 +22,15 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def new
+		authorize! :read, @user
+	end
+
 	def update
 		if @user.update(user_params)
 			redirect_to root_path
-		else
+		elsif @user.sign_up_complete?
 			render :edit
-		end
-	end
-
-	def new
-		beta_code = BetaCode.find_by(auth_token: params[:code])
-		return redirect_to root_path, notice: "Please sign up for the beta." unless beta_code && !beta_code.used
-
-		@user = User.new(email: params[:email])
-
-		respond_to do |format|
-			format.html
-			format.js
-		end
-	end
-
-	def create
-		@user = User.new(user_params)
-
-		if @user.save
-			BetaCode.find_by(auth_token: params[:code]).update_column(:used, true)
-			log_in(@user)
-			redirect_to root_path
 		else
 			render :new
 		end
@@ -77,6 +60,6 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :email, :address, :password, :password_confirmation, :address, :category, :age_group, :latitude, :longitude, :code, notification_settings: [:join, :proposal, :mention, :personal, :expand])
+		params.require(:user).permit(:name, :email, :address, :category, :age_group, :latitude, :longitude, :sign_up_complete, notification_settings: [:join, :proposal, :mention, :personal, :expand])
 	end
 end
