338b354405916c70c5f8f142903c26a5f8883935
baron816 <baron.willeford@gmail.com>
2016-01-27 14:43:23 -0500

    fixes sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,22 +1,54 @@
 require 'rails_helper'
 
 describe SessionsController do
-  let!(:user) { create(:baron) }
+
 
   describe "#POST create" do
-    context "user enters correct credentials" do
+    context "user doesn't exist yet" do
       before do
-        omniauth_headers(user)
-        post :create
+        omniauth_headers({provider: "facebook", uid: "12355", info: {email: "myEmail@mail.com", name: "Dude McAwesome"}})
       end
 
-      it "redirects to the root" do
+      it "creates a new user" do
+        expect { post :create, provider: :facebook }.to change(User, :count).by(1)
+      end
+
+      it "redirects to new_user_path" do
+        post :create, provider: :facebook
         expect(response).to redirect_to(new_user_path)
+      end
+
+      it "logs in the new user" do
+        post :create, provider: :facebook
+        expect(cookies.signed[:auth_token]).to eq(User.last.auth_token)
+      end
+    end
+
+    context "user exists" do
+      let!(:user) { create(:baron) }
+
+      before do
+        omniauth_headers(omniauth_mock_attributes(user))
+      end
+
+      it "does not create a new user" do
+        expect { post :create, provider: :facebook }.to change(User, :count).by(0)
+      end
+
+      it "redirects to root" do
+        post :create, provider: :facebook
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "signs in the user" do
+        post :create, provider: :facebook
+        expect(cookies.signed[:auth_token]).to eq(user.auth_token)
       end
     end
   end
 
   describe "#DELETE destroy" do
+    let!(:user) { create(:baron) }
     before do
       log_in(user)
       delete :destroy
