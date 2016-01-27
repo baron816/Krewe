d6fb0a70bf937409eeee5438731605e4a26e14c3
baron816 <baron.willeford@gmail.com>
2016-01-27 11:29:50 -0500

    removes fields handled through omniauth and adds uid and provider fields

---
+++ db/migrate/20160127151118_fix_is_admin_for_users.rb
@@ -0,0 +1,6 @@
+class FixIsAdminForUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :is_admin, :boolean
+    add_column :users, :is_admin, :boolean, default: false
+  end
+end
