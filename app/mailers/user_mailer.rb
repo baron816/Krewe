e13d037aca4431e0db2db0249051d4d8ed829874
baron816 <baron.willeford@gmail.com>
2016-01-27 11:40:04 -0500

    no need for beta code or password reset mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,5 @@
 class UserMailer < ApplicationMailer
 
-  def beta_code(beta_code)
-    @beta_code = beta_code
-
-    mail to: @beta_code.email, subject: "Finish Signing Up For Krewe"
-  end
-
-  def password_reset(user)
-    @user = user
-
-    mail to: @user.email, subject: "Reset Krewe Password"
-  end
-
   def user_message_alert(message)
     @message = message
     messageable = @message.messageable
