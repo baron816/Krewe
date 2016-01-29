29490be5539068c61e797dc3e04b5aedcf6fe55f
baron816 <baron.willeford@gmail.com>
2016-01-29 18:54:21 -0500

    adds index for uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127163247) do
+ActiveRecord::Schema.define(version: 20160129232629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,5 +181,6 @@ ActiveRecord::Schema.define(version: 20160127163247) do
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
 end
