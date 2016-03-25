3cfb1a4d0b9d0eba243a4c5db6ec3ad9e6d6dca2
baron816 <baron.willeford@gmail.com>
2016-03-25 12:33:00 -0400

    adds tests for new users controller methods

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,48 @@ describe UsersController do
   let!(:user2) { create(:user_wtc) }
   let!(:group) { Group.first }
 
+  describe "GET #new" do
+    before do
+      get :new
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template(:new)
+    end
+
+    it "creates a new user" do
+      expect(assigns[:user]).to be_a_new(User)
+    end
+  end
+
+  describe "POST #create" do
+    context "valid password" do
+      let!(:user_attributes) { attributes_for(:email_provider, password: "mdsgvzj48*()") }
+
+      it "creates a new user" do
+        expect { post :create, user: user_attributes }.to change(User, :count).by(1)
+      end
+
+      it "redirects to verify_email users path" do
+        post :create, user: user_attributes
+        expect(response).to redirect_to(verify_email_users_path)
+      end
+    end
+
+    context "invalid password" do
+      let!(:invalid_password_user_attributes) { attributes_for(:email_provider, password: "password")}
+
+      it "does not save the user" do
+        expect { post :create, user: invalid_password_user_attributes }.to change(User, :count).by(0)
+      end
+
+      it "renders new template" do
+        post :create, user: invalid_password_user_attributes
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
   describe "GET #show" do
     context "no user" do
       before do
@@ -89,19 +131,87 @@ describe UsersController do
 
 
   describe "GET #complete_sign_up" do
-    let(:user) { create(:user_home) }
-    before do
+    context "needs verification" do
+      let(:user) { create(:user_home) }
+      before do
+        log_in(user)
+        get :complete_sign_up
+      end
+
+      it "makes a new user" do
+      	expect(assigns[:user]).to eq(user)
+      end
+
+      it "renders new template" do
+      	expect(response).to redirect_to(verify_email_users_path)
+      end
+    end
+
+    context "does not need verification" do
+      let(:user) { create(:user_home, email_verified: true) }
+      before do
+        log_in(user)
+        get :complete_sign_up
+      end
+
+      it "renders new template" do
+        expect(response).to render_template(:complete_sign_up)
+      end
+    end
+  end
+
+  describe "#GET verify_email" do
+    it "redirects to root if sign up is complete" do
       log_in(user)
-      get :complete_sign_up
+      get :verify_email
+      expect(response).to redirect_to root_path
     end
 
-    it "makes a new user" do
-    	expect(assigns[:user]).to eq(user)
+    it "renders template if sign up is not complete" do
+      user = create(:user_home, sign_up_complete: false)
+      log_in(user)
+      get :verify_email
+      expect(response).to render_template(:verify_email)
+    end
+  end
+
+  describe "#GET email_confirmed" do
+    it "the user's email is not verified" do
+      expect(user.email_verified?).to eq(false)
     end
 
-    it "renders new template" do
-    	expect(response).to render_template(:complete_sign_up)
+    context "user has correct code" do
+      before do
+        get :email_confirmed, id: user, code: user.password_reset_token
+      end
+
+      it "logs in the user" do
+        expect(cookies.signed[:auth_token]).to eq(user.auth_token)
+      end
+
+      it "sets email_verified to true" do
+        user.reload
+        expect(user.email_verified?).to eq(true)
+      end
+
+      it "redirects to complete_sign_up_users_path" do
+        expect(response).to redirect_to(complete_sign_up_users_path)
+      end
     end
+
+    context "user has wrong code" do
+      before do
+        get :email_confirmed, id: user, code: '12345'
+      end
+
+      it "redirect to root path" do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#PATCH update_email" do
+
   end
 
   describe "#DELETE #destroy" do
@@ -124,7 +234,7 @@ describe UsersController do
       expect(DropUserVote.count).to eq(0)
     end
 
-    it "rediects to a new survey" do
+    it "redirects to a new survey" do
       expect(response).to redirect_to new_survey_path(email: user.email)
     end
   end
