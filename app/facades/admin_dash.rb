58cf3c2fac0dff61329bc96d3dc7448dc16acbb6
baron816 <baron.willeford@gmail.com>
2016-04-09 16:46:03 -0400

    admin dash shows completed sign up count and ratio

--- app/facades/admin_dash.rb
+++ app/facades/admin_dash.rb
@@ -15,4 +15,12 @@ class AdminDash
   def users_count
     User.count
   end
+
+  def completed_sign_up_count
+    User.where(sign_up_complete: true).count
+  end
+
+  def completion_ratio
+    ((completed_sign_up_count.to_f / users_count.to_f) * 100).round(2)
+  end
 end
