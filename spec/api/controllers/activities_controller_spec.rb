ef22499b74e0c863f8be4c08d83fa625832de748
Baron Willeford <baron.willeford@gmail.com>
2016-06-21 15:25:54 -0400

    uncomments api controller tests

--- spec/api/controllers/activities_controller_spec.rb
+++ spec/api/controllers/activities_controller_spec.rb
@@ -1,126 +1,126 @@
-# require 'rails_helper'
-#
-# RSpec.describe Api::V1::ActivitiesController, type: :controller do
-#   before do
-#     @user = create(:user_home)
-#     api_authorization_header(@user.auth_token)
-#     @group = @user.groups.first
-#   end
-#
-#   describe "POST #create" do
-#     context "when successful" do
-#       before do
-#         @activity_attributes = attributes_for(:activity_future)
-#         @activity_attributes[:group_id] = @group.id
-#         @activity_attributes[:proposer_id] = @user.id
-#         post :create, { activity: @activity_attributes, group_id: @group }
-#       end
-#
-#       it "is successful" do
-#         expect(response).to have_http_status(201)
-#       end
-#
-#       it "returns the created activity" do
-#         expect(json_response[:location]).to eq(@activity_attributes[:location])
-#       end
-#     end
-#
-#     context "when not successful" do
-#       before do
-#         @activity_attributes = attributes_for(:activity_future)
-#         @activity_attributes[:location] = nil
-#         post :create, { activity: @activity_attributes, group_id: @group}
-#       end
-#
-#       it "is not succsssful" do
-#         expect(response).to have_http_status(422)
-#       end
-#
-#       it "has an errors json" do
-#         expect(json_response).to have_key(:errors)
-#       end
-#
-#       it "indicates the error" do
-#         expect(json_response[:errors][:location]).to eq(["can't be blank"])
-#       end
-#     end
-#   end
-#
-#   before do
-#     @activity = build(:activity_future)
-#     @activity.group = @group
-#     @activity.save
-#   end
-#
-#   describe "GET #show" do
-#     before do
-#       get :show, id: @activity, group_id: @group
-#     end
-#
-#     it "is successful" do
-#       expect(response).to have_http_status(200)
-#     end
-#
-#     it "returns the json representation of the activity" do
-#       expect(json_response[:location]).to eq(@activity.location)
-#     end
-#   end
-#
-#   describe "PUT/PATCH #update" do
-#     context "when succsssfully updated" do
-#       before do
-#         patch :update, { id: @activity, group_id: @group, activity: { location: "Empire State Building"} }
-#       end
-#
-#       it "is successful" do
-#         expect(response).to have_http_status(200)
-#       end
-#
-#       it "renders the json representation with the updated activity" do
-#         expect(json_response[:location]).to eq("Empire State Building")
-#       end
-#     end
-#
-#     context "when update is unsuccessful" do
-#       before do
-#         patch :update, { id: @activity, group_id: @group, activity: { location: nil } }
-#       end
-#
-#       it "is not successful" do
-#         expect(response).to have_http_status(422)
-#       end
-#
-#       it "has an errors json" do
-#         expect(json_response).to have_key(:errors)
-#       end
-#
-#       it "says what the problem is" do
-#         expect(json_response[:errors][:location]).to eq(["can't be blank"])
-#       end
-#     end
-#   end
-#
-#   describe "POST #add_user" do
-#     before do
-#       post :add_user, id: @activity, group_id: @group
-#     end
-#
-#     it "is successful" do
-#       expect(response).to have_http_status(201)
-#     end
-#
-#     it "returns the activity" do
-#       expect(json_response[:location]).to eq(@activity.location)
-#     end
-#   end
-#
-#   describe "DELETE #remove_user" do
-#     before do
-#       delete :remove_user, { id: @activity, group_id: @group }
-#     end
-#
-#     it "is successful" do
-#       expect(response).to have_http_status(204)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Api::V1::ActivitiesController, type: :controller do
+  before do
+    @user = create(:user_home)
+    api_authorization_header(@user.auth_token)
+    @group = @user.groups.first
+  end
+
+  describe "POST #create" do
+    context "when successful" do
+      before do
+        @activity_attributes = attributes_for(:activity_future)
+        @activity_attributes[:group_id] = @group.id
+        @activity_attributes[:proposer_id] = @user.id
+        post :create, { activity: @activity_attributes, group_id: @group }
+      end
+
+      it "is successful" do
+        expect(response).to have_http_status(201)
+      end
+
+      it "returns the created activity" do
+        expect(json_response[:location]).to eq(@activity_attributes[:location])
+      end
+    end
+
+    context "when not successful" do
+      before do
+        @activity_attributes = attributes_for(:activity_future)
+        @activity_attributes[:location] = nil
+        post :create, { activity: @activity_attributes, group_id: @group}
+      end
+
+      it "is not succsssful" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "has an errors json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "indicates the error" do
+        expect(json_response[:errors][:location]).to eq(["can't be blank"])
+      end
+    end
+  end
+
+  before do
+    @activity = build(:activity_future)
+    @activity.group = @group
+    @activity.save
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, id: @activity, group_id: @group
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the json representation of the activity" do
+      expect(json_response[:location]).to eq(@activity.location)
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    context "when succsssfully updated" do
+      before do
+        patch :update, { id: @activity, group_id: @group, activity: { location: "Empire State Building"} }
+      end
+
+      it "is successful" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the json representation with the updated activity" do
+        expect(json_response[:location]).to eq("Empire State Building")
+      end
+    end
+
+    context "when update is unsuccessful" do
+      before do
+        patch :update, { id: @activity, group_id: @group, activity: { location: nil } }
+      end
+
+      it "is not successful" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "has an errors json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "says what the problem is" do
+        expect(json_response[:errors][:location]).to eq(["can't be blank"])
+      end
+    end
+  end
+
+  describe "POST #add_user" do
+    before do
+      post :add_user, id: @activity, group_id: @group
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(201)
+    end
+
+    it "returns the activity" do
+      expect(json_response[:location]).to eq(@activity.location)
+    end
+  end
+
+  describe "DELETE #remove_user" do
+    before do
+      delete :remove_user, { id: @activity, group_id: @group }
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
--- spec/api/controllers/groups_controller_spec.rb
+++ spec/api/controllers/groups_controller_spec.rb
@@ -1,33 +1,33 @@
-# require 'rails_helper'
-#
-# RSpec.describe Api::V1::GroupsController, type: :controller do
-#   before do
-#     user = create(:user_home)
-#     api_authorization_header(user.auth_token)
-#     @group = user.groups.first
-#   end
-#
-#   describe "GET #show" do
-#     before do
-#       get :show, id: @group
-#     end
-#
-#     it "is successful" do
-#       expect(response).to have_http_status(200)
-#     end
-#
-#     it "returns the correct json data" do
-#       expect(json_response[:name]).to eq(@group.name)
-#     end
-#   end
-#
-#   describe "DELETE #drop_user" do
-#     before do
-#       delete :drop_user, id: @group
-#     end
-#
-#     it "is successful" do
-#       expect(response).to have_http_status(204)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Api::V1::GroupsController, type: :controller do
+  before do
+    user = create(:user_home)
+    api_authorization_header(user.auth_token)
+    @group = user.groups.first
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, id: @group
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the correct json data" do
+      expect(json_response[:name]).to eq(@group.name)
+    end
+  end
+
+  describe "DELETE #drop_user" do
+    before do
+      delete :drop_user, id: @group
+    end
+
+    it "is successful" do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
